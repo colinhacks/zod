af94692b435cdb89a879d782b8ce023a394c6c8e
Colin McDonnell <colinmcd@alum.mit.edu>
2020-04-07 12:45:21 -0700

    Added files

---
+++ src/types/date.ts
@@ -0,0 +1,21 @@
+import * as z from './base';
+import { ZodUndefined } from './undefined';
+import { ZodNull } from './null';
+import { ZodUnion } from './union';
+
+export interface ZodDateDef extends z.ZodTypeDef {
+  t: z.ZodTypes.date;
+}
+
+export class ZodDate extends z.ZodType<Date, ZodDateDef> {
+  optional: () => ZodUnion<[this, ZodUndefined]> = () => ZodUnion.create([this, ZodUndefined.create()]);
+
+  nullable: () => ZodUnion<[this, ZodNull]> = () => ZodUnion.create([this, ZodNull.create()]);
+
+  toJSON = () => this._def;
+  static create = (): ZodDate => {
+    return new ZodDate({
+      t: z.ZodTypes.date,
+    });
+  };
+}
