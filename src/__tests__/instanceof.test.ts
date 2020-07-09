4dc9a0ecd3f027c04e20dfca5d91eade8bcf261f
Colin McDonnell <colinmcd@alum.mit.edu>
2020-07-08 18:47:47 -0700

    Added instanceof tests

---
+++ src/__tests__/instanceof.test.ts
@@ -0,0 +1,27 @@
+import * as z from '..';
+import { util } from '../helpers/util';
+
+test('instanceof', () => {
+  class Test {}
+  class Subtest extends Test {}
+
+  const TestSchema = z.instanceof(Test);
+  const SubtestSchema = z.instanceof(Subtest);
+
+  TestSchema.parse(new Test());
+  TestSchema.parse(new Subtest());
+  SubtestSchema.parse(new Subtest());
+
+  expect.assertions(4);
+  expect(() => SubtestSchema.parse(new Test())).toThrow();
+  expect(() => TestSchema.parse(12)).toThrow();
+  TestSchema.parseAsync(12).catch(err => {
+    expect(err.errors[0].message).toEqual('Input not instance of Test');
+  });
+  SubtestSchema.parseAsync(12).catch(err => {
+    expect(err.errors[0].message).toEqual('Input not instance of Subtest');
+  });
+
+  const f1: util.AssertEqual<Test, z.infer<typeof TestSchema>> = true;
+  f1;
+});
