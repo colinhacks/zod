13c18c0b63e997b7291231c4e2b77da72300bc26
Colin McDonnell <colinmcd@alum.mit.edu>
2020-07-18 12:47:14 -0700

    Better error message for undefined inputs

--- src/playground.ts
+++ src/playground.ts
@@ -1,58 +1,23 @@
-import * as z from '.';
+// import * as z from '.';
 
-const FormData = z
-  .object({
-    email: z.string().email(),
-    password: z.string().min(10),
-    confirm: z.string().min(10),
-  })
-  .refine(obj => obj.password === obj.confirm, {
-    message: 'Passwords do not match',
-    path: ['confirm'], // sets the path of the error thrown by this refinement
-  });
+// const ZodRHF = <T extends z.ZodType<any,any>>(schema:T)=>(values:any)=>{
+//   try{
+//     schema.parse(values)
+//     return {}
+//   }catch(err){
+//     return (err as z.ZodError).formErrors.fieldErrors;
+//   }
+// }
 
-try {
-  FormData.parse({
-    email: 'not an email',
-    password: 'tooshort',
-    confirm: 'nomatch',
-  });
-} catch (err) {
-  if (!(err instanceof z.ZodError)) throw err;
+// z.string()
+//   .parseAsync(undefined)
+//   .catch(err => console.log(JSON.stringify(err.errors, null, 2)));
 
-  console.log(err.errors);
-  /*
-  [
-    { code: 'invalid_string', validation: 'email', path: ['email'], message: 'Invalid email' },
-    {
-      code: 'too_small',
-      minimum: 10,
-      type: 'string',
-      inclusive: true,
-      path: ['password'],
-      message: 'Should be at least 10 characters',
-    },
-    {
-      code: 'too_small',
-      minimum: 10,
-      type: 'string',
-      inclusive: true,
-      path: ['confirm'],
-      message: 'Should be at least 10 characters',
-    },
-    { code: 'custom_error', message: 'Passwords do not match', path: ['confirm'] },
-  ]; 
-  */
-
-  console.log(err.formErrors);
-  /*
-    {
-    formErrors: [],
-    fieldErrors: {
-      email: ['Invalid email'],
-      password: ['Should be at least 10 characters'],
-      confirm: ['Should be at least 10 characters', 'Passwords do not match'],
-    },
-  }
-  */
-}
+// export const errorMap: z.ZodErrorMap = (err, ctx) => {
+//   if (err.code === z.ZodErrorCode.invalid_type) {
+//     if (err.received === 'undefined') {
+//       return { message: 'Required.' };
+//     }
+//   }
+//   return { message: ctx.defaultError };
+// };
