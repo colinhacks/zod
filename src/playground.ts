7a9c6ed2bbf52d66d6f3504a076e939ebbd1b45d
Colin McDonnell <colinmcd@alum.mit.edu>
2020-08-28 16:17:23 -0700

    Default function type: void => unknown

---
+++ src/playground.ts
@@ -0,0 +1,11 @@
+// import * as z from '.';
+
+// const asdf = z
+//   .function()
+//   .args(z.string())
+//   .returns(z.unknown())
+//   .implement(arg => {
+//     return arg.length;
+//   });
+
+// type asdf = string extends unknown ? true : false;
