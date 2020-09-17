5b864362b700c8c7122301a60909dba53b39dd5a
Colin McDonnell <colinmcd@alum.mit.edu>
2020-09-16 17:13:15 -0700

    Added nullable file

---
+++ src/types/nullable.ts
@@ -0,0 +1,35 @@
+import * as z from './base';
+
+export interface ZodNullableDef<T extends z.ZodTypeAny = z.ZodTypeAny>
+  extends z.ZodTypeDef {
+  t: z.ZodTypes.nullable;
+  innerType: T;
+}
+
+// This type allows for nullable flattening
+export type ZodNullableType<T extends z.ZodTypeAny> = T extends ZodNullable<
+  z.ZodTypeAny
+>
+  ? T
+  : ZodNullable<T>;
+
+export class ZodNullable<T extends z.ZodTypeAny> extends z.ZodType<
+  T['_output'] | null,
+  ZodNullableDef<T>,
+  T['_input'] | null
+> {
+  // An nullable nullable is the original nullable
+  // nullable: () => ZodNullableType<this> = () => this as ZodNullableType<this>;
+  toJSON = () => ({
+    t: this._def.t,
+    innerType: this._def.innerType.toJSON(),
+  });
+
+  static create = <T extends z.ZodTypeAny>(type: T): ZodNullableType<T> => {
+    if (type instanceof ZodNullable) return type as ZodNullableType<T>;
+    return new ZodNullable({
+      t: z.ZodTypes.nullable,
+      innerType: type,
+    }) as ZodNullableType<T>;
+  };
+}
