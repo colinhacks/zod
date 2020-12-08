b073412a9fcdc905a22a945d99e1a500304fd10d
Colin McDonnell <colinmcd@alum.mit.edu>
2020-12-07 18:33:36 -0800

    Added test for this binding.

--- src/__tests__/base.test.ts
+++ src/__tests__/base.test.ts
@@ -15,3 +15,14 @@ test('type guard', () => {
     f1;
   }
 });
+
+test('test this binding', () => {
+  const callback = (predicate: (val: string) => boolean) => {
+    return predicate('hello');
+  };
+
+  expect(callback(z.string().is)).toBe(true); // true
+  expect(callback(value => z.string().is(value))).toBe(true); // true
+  expect(callback(z.string().check)).toBe(true); // true
+  expect(callback(value => z.string().check(value))).toBe(true); // true
+});
--- src/playground.ts
+++
@@ -1,8 +0,0 @@
-import * as z from '.';
-
-const callback = (predicate: (val: string) => boolean) => {
-  return predicate('hello');
-};
-
-console.log(callback(z.string().check)); // false!?
-console.log(callback(value => z.string().check(value))); // true
