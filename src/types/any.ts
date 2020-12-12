c3dc9cd417901cd4bea75fe6c989e264b5af5c98
Olzhas Alexandrov <olzhas@alexandrov.co>
2020-12-12 02:58:37 -0600

    Fixed import

--- src/types/any.ts
+++ src/types/any.ts
@@ -1,5 +1,5 @@
 import { ZodTypes } from "../ZodTypes";
-import { ZodType, ZodTypeDef } from "./base";
+import { ZodType, ZodTypeDef } from "./base/type";
 // import { ZodUndefined } from './undefined';
 // import { ZodNull } from './null';
 // import { ZodUnion } from './union';
