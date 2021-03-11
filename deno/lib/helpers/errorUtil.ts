7d5cd4cdbfaf79890128dcfd231b530a1b8806c9
Colin McDonnell <colinmcd@alum.mit.edu>
2021-03-10 18:55:57 -0700

    removed deno

--- deno/lib/helpers/errorUtil.ts
+++
@@ -1,5 +0,0 @@
-export namespace errorUtil {
-  export type ErrMessage = string | { message?: string };
-  export const errToObj = (message?: ErrMessage) =>
-    typeof message === "string" ? { message } : message || {};
-}
