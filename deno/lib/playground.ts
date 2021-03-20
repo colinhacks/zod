a5d6c6589a486777e8ca5d017d147a06d0a94541
Colin McDonnell <colinmcd@alum.mit.edu>
2021-03-20 17:42:40 -0600

    Testing precommit

--- deno/lib/playground.ts
+++ deno/lib/playground.ts
@@ -1,17 +1,7 @@
-import { string, z } from "./index.ts";
+import { z } from "./index.ts";
 
 const run = async () => {
   z;
-  const arg = z
-    .object({ name: string() })
-    .refine((val) => val.name.length > 10)
-    .and(z.object({ test: z.number() }));
-  console.log(
-    arg.safeParse({
-      name: "asdf",
-      test: 1234,
-    })
-  );
 };
 
 run();
--- src/playground.ts
+++ src/playground.ts
@@ -1,18 +1,7 @@
-import { string, z } from "./index";
+import { z } from "./index";
 
 const run = async () => {
   z;
-  const arg = z
-    .object({ name: string() })
-    .refine((val) => val.name.length > 10)
-    .and(z.object({ test: z.number() }));
-
-  console.log(
-    arg.safeParse({
-      name: "aasdfsdffssdf",
-      test: 1234,
-    })
-  );
 };
 
 run();
