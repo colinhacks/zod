6e74f877997c25de52255d71af850a59afbf7e2d
Colin McDonnell <colinmcd@alum.mit.edu>
2021-03-21 21:05:53 -0600

    Removed map files

--- src/external.js
+++
@@ -1,4 +0,0 @@
-export * from "./helpers/parseUtil";
-export * from "./types";
-export * from "./ZodError";
-//# sourceMappingURL=external.js.map
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import * as z from "./external";
-export * from "./external";
-export { z };
-//# sourceMappingURL=index.js.map
