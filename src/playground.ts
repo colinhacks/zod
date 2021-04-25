edc8479cddafdc975e526a9f35917331f180503d
Colin McDonnell <colinmcd@alum.mit.edu>
2021-04-25 00:49:28 -0400

    Fixed tuple parsing bug when tupledata length is different from ZodTuple.items length

--- src/playground.ts
+++ src/playground.ts
@@ -1,7 +1,33 @@
 import { z } from "./index";
 
 const run = async () => {
-  z;
+  const geojsonPolygonSchema = z.object({
+    type: z.literal("Polygon"),
+    coordinates: z.array(z.array(z.tuple([z.number(), z.number()]))),
+  });
+
+  const geojsonMultiPolygonSchema = z.object({
+    type: z.literal("MultiPolygon"),
+  });
+
+  const geojsonShapeSchema = z.union([
+    geojsonPolygonSchema,
+    geojsonMultiPolygonSchema,
+  ]);
+
+  const value = {
+    type: "MultiPolygon",
+    coordinates: [
+      [
+        [
+          [0, 1],
+          [2, 3],
+          [4, 5],
+        ],
+      ],
+    ],
+  };
+  console.log(geojsonShapeSchema.safeParse(value));
 };
 
 run();
