d610e4d8779fd0d8ff744881b1a8afc2bf50ffde
Colin McDonnell <colinmcd@alum.mit.edu>
2021-05-01 23:26:34 -0400

    3.0.0-alpha.40

--- deno/lib/playground.ts
+++ deno/lib/playground.ts
@@ -8,6 +8,18 @@ const run = async () => {
   schema.parse("abc", {
     errorMap: () => ({ message: "custom error message" }),
   });
+
+  type B = {
+    name: string | null;
+    child: B | null;
+  };
+
+  const bSchema: z.ZodSchema<B, any, any> = z.lazy(() =>
+    z.object({
+      name: z.string().nullable().default(null),
+      child: bSchema.nullable(),
+    })
+  );
 };
 
 run();
