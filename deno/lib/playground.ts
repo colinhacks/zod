350490ddc1965153f4db636396e9f8b1ebea6532
Colin McDonnell <colinmcd@alum.mit.edu>
2021-05-15 18:14:08 -0400

    Clear playground

--- deno/lib/playground.ts
+++ deno/lib/playground.ts
@@ -2,10 +2,6 @@ import { z } from "./index.ts";
 
 const run = async () => {
   z;
-
-  // const schema = z.string().refine((val) => val.length === 5);
-  // const result = schema.safeParse(undefined);
-  // console.log(result);
 };
 
 run();
