89088c2570230d9dd17e57c5e795b9babba69c57
Matti Lankinen <m.lankinen@iki.fi>
2021-06-13 21:36:27 +0300

    Use switch-case to speedup parsed type detection

--- src/helpers/parseUtil.ts
+++ src/helpers/parseUtil.ts
@@ -35,38 +35,44 @@ export const ZodParsedType = util.arrayToEnum([
 export type ZodParsedType = keyof typeof ZodParsedType;
 
 export const getParsedType = (data: any): ZodParsedType => {
-  if (typeof data === "string") return ZodParsedType.string;
-  if (typeof data === "number") {
-    if (Number.isNaN(data)) return ZodParsedType.nan;
-    return ZodParsedType.number;
+  const t = typeof data;
+  switch (t) {
+    case "undefined":
+      return ZodParsedType.undefined;
+    case "string":
+      return ZodParsedType.string;
+    case "number":
+      return Number.isNaN(data) ? ZodParsedType.nan : ZodParsedType.number;
+    case "boolean":
+      return ZodParsedType.boolean;
+    case "function":
+      return ZodParsedType.function;
+    case "bigint":
+      return ZodParsedType.bigint;
+    case "object":
+      if (Array.isArray(data)) return ZodParsedType.array;
+      if (data === null) return ZodParsedType.null;
+      if (
+        data.then &&
+        typeof data.then === "function" &&
+        data.catch &&
+        typeof data.catch === "function"
+      ) {
+        return ZodParsedType.promise;
+      }
+      if (data instanceof Map) {
+        return ZodParsedType.map;
+      }
+      if (data instanceof Set) {
+        return ZodParsedType.set;
+      }
+      if (data instanceof Date) {
+        return ZodParsedType.date;
+      }
+      return ZodParsedType.object;
+    default:
+      return ZodParsedType.unknown;
   }
-  if (typeof data === "boolean") return ZodParsedType.boolean;
-  if (typeof data === "bigint") return ZodParsedType.bigint;
-  if (typeof data === "symbol") return ZodParsedType.symbol;
-  if (data instanceof Date) return ZodParsedType.date;
-  if (typeof data === "function") return ZodParsedType.function;
-  if (data === undefined) return ZodParsedType.undefined;
-  if (typeof data === "undefined") return ZodParsedType.undefined;
-  if (typeof data === "object") {
-    if (Array.isArray(data)) return ZodParsedType.array;
-    if (data === null) return ZodParsedType.null;
-    if (
-      data.then &&
-      typeof data.then === "function" &&
-      data.catch &&
-      typeof data.catch === "function"
-    ) {
-      return ZodParsedType.promise;
-    }
-    if (data instanceof Map) {
-      return ZodParsedType.map;
-    }
-    if (data instanceof Set) {
-      return ZodParsedType.set;
-    }
-    return ZodParsedType.object;
-  }
-  return ZodParsedType.unknown;
 };
 
 export const makeIssue = (
