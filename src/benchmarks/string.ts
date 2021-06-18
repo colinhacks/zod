1d2c364ce8f5f7b92bda59f002bebfd83c3c1856
Colin McDonnell <colinmcd@alum.mit.edu>
2021-06-18 15:59:24 -0400

    FMC

---
+++ src/benchmarks/string.ts
@@ -0,0 +1,44 @@
+import Benchmark from "benchmark";
+
+import { z } from "../index";
+
+const SUITE_NAME = "z.string";
+const suite = new Benchmark.Suite(SUITE_NAME);
+
+const empty = "";
+const short = "short";
+const long = "long".repeat(256);
+const manual = (str: unknown) => {
+  if (typeof str !== "string") {
+    throw new Error("Not a string");
+  }
+
+  return str;
+};
+const stringSchema = z.string();
+
+suite
+  .add("empty string", () => {
+    stringSchema.parse(empty);
+  })
+  .add("short string", () => {
+    stringSchema.parse(short);
+  })
+  .add("long string", () => {
+    stringSchema.parse(long);
+  })
+  .add("invalid: null", () => {
+    try {
+      stringSchema.parse(null);
+    } catch (err) {}
+  })
+  .add("manual parser: long", () => {
+    manual(long);
+  })
+  .on("cycle", (e: Benchmark.Event) => {
+    console.log(`${SUITE_NAME}: ${e.target}`);
+  });
+
+export default {
+  suites: [suite],
+};
