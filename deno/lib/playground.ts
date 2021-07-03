5e4b9e13e1018ee8a2886f2f0e26e1edecffdaae
Colin McDonnell <colinmcd@alum.mit.edu>
2021-07-03 16:38:40 -0400

    Implement deep intersection merge

--- deno/lib/playground.ts
+++ deno/lib/playground.ts
@@ -1,13 +1,26 @@
 import { z } from "./index.ts";
 
-class Test {
-  constructor(public readonly path: string) {}
-}
-
-const test = new Test("asdf");
-console.log(test.path);
 const run = async () => {
   z;
+
+  const Animal = z.object({
+    properties: z.object({
+      is_animal: z.boolean(),
+    }),
+  });
+  const Cat = z
+    .object({
+      properties: z.object({
+        jumped: z.boolean(),
+      }),
+    })
+    .and(Animal);
+
+  type Cat = z.infer<typeof Cat>;
+  // const cat:Cat = 'asdf' as any;
+  const cat = Cat.parse({ properties: { is_animal: true, jumped: true } });
+  console.log(cat.properties);
+  console.log(cat.properties);
 };
 
 run();
