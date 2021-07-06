a233ca87bf888f5b821bc8ae8ae3343c1edba9e2
Colin McDonnell <colinmcd@alum.mit.edu>
2021-07-06 12:24:02 -0400

    Clear playgrond

--- deno/lib/playground.ts
+++ deno/lib/playground.ts
@@ -2,28 +2,6 @@ import { z } from "./index.ts";
 
 const run = async () => {
   z;
-  const Strings = z.array(z.string()).superRefine((val, ctx) => {
-    if (val.length > 3) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.too_big,
-        maximum: 3,
-        type: "array",
-        inclusive: true,
-        message: "Too many items 😡",
-      });
-    }
-
-    if (val.length !== new Set(val).size) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: `No duplicates allowed.`,
-      });
-    }
-  });
-  console.log(Strings.safeParse(["asfd", "qwer"]));
-
-  // console.log(Strings.safeParse(["asfd", "asfd", "asfd", "asfd"]));
-  // console.log(Strings.safeParse(["asfd", "asfd"]));
 };
 run();
 
