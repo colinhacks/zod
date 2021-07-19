224aa0d3e0e568ff619748d8d46570a51737425d
YimingIsCOLD <yimingpeh@gmail.com>
2021-07-19 12:47:51 +0800

    Combined boolean required & invalid test

--- src/__tests__/boolean.test.ts
+++ src/__tests__/boolean.test.ts
@@ -3,14 +3,11 @@ import { expect, test } from "@jest/globals";
 
 import * as z from "../index";
 
-test("required type validations", () => {
-  const message = "This field is required";
-  const required = z.boolean({ required: message });
-  expect(() => required.parse(undefined)).toThrow(message);
-});
+test("required & invalid validations", () => {
+  const requiredMessage = "This field is required";
+  const invalidMessage = "Expected number, instead of whatever you input!";
 
-test("invalid type validations", () => {
-  const message = "Expected boolean, instead of whatever you input!";
-  const invalid = z.boolean({ invalid: message });
-  expect(() => invalid.parse("false")).toThrow(message);
+  const b = z.boolean({ invalid: invalidMessage, required: requiredMessage });
+  expect(() => b.parse("false")).toThrow(invalidMessage);
+  expect(() => b.parse(undefined)).toThrow(requiredMessage);
 });
