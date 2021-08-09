f322ad74cbfa4156d86b9b1a1ceda0942fb14036
Scott <scott@scotttrinh.com>
2021-08-09 11:43:36 -0400

    chore: automatic fixes and code generation for 5cc04ad9 (Add benchmarks f…)
    https://github.com/zod-contrib/zod/actions/runs/1113723197

--- deno/lib/playground.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "./index.ts";
-
-const maybeShortStr = z.string().min(5).or(z.undefined());
-
-console.log(maybeShortStr.safeParse(undefined));
-console.log(maybeShortStr.safeParse("1234"));
-console.log(maybeShortStr.safeParse("12345"));
