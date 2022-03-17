8d1bb1e7825c3c6dab091e5f6fb2127a110ec20f
Tom MacWright <tom@macwright.org>
2022-03-17 18:01:15 -0400

    Add realworld benchmark

--- deno/lib/benchmarks/index.ts
+++ deno/lib/benchmarks/index.ts
@@ -1,9 +1,11 @@
 import discriminatedUnionBenchmarks from "./discriminatedUnion.ts";
 import objectBenchmarks from "./object.ts";
+import realworld from "./realworld.ts";
 import stringBenchmarks from "./string.ts";
 import unionBenchmarks from "./union.ts";
 
 for (const suite of [
+  ...realworld.suites,
   ...stringBenchmarks.suites,
   ...objectBenchmarks.suites,
   ...unionBenchmarks.suites,
--- src/benchmarks/index.ts
+++ src/benchmarks/index.ts
@@ -1,9 +1,11 @@
 import discriminatedUnionBenchmarks from "./discriminatedUnion";
 import objectBenchmarks from "./object";
+import realworld from "./realworld";
 import stringBenchmarks from "./string";
 import unionBenchmarks from "./union";
 
 for (const suite of [
+  ...realworld.suites,
   ...stringBenchmarks.suites,
   ...objectBenchmarks.suites,
   ...unionBenchmarks.suites,
