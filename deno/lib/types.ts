85fc7d72926349d50aef39436dc65eba20f1c79e
santosmarco <eu@marco.rio.br>
2022-10-09 11:17:17 -0300

    Add lazy to root Zod class

--- deno/lib/types.ts
+++ deno/lib/types.ts
@@ -368,6 +368,8 @@ export abstract class ZodType<
     this.and = this.and.bind(this);
     this.transform = this.transform.bind(this);
     this.default = this.default.bind(this);
+    this.brand = this.brand.bind(this);
+    this.lazy = this.lazy.bind(this);
     this.describe = this.describe.bind(this);
     this.isNullable = this.isNullable.bind(this);
     this.isOptional = this.isOptional.bind(this);
@@ -425,6 +427,10 @@ export abstract class ZodType<
       type: this,
       ...processCreateParams(undefined),
     });
+  }
+
+  lazy(): ZodLazy<this> {
+    return ZodLazy.create(() => this);
   }
 
   describe(description: string): this {
