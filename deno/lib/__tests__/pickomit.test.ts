bd0c3003651c1d60dfe18159aa56febcaf96f696
Igal Klebanov <igalklebanov@gmail.com>
2022-11-16 23:14:58 +0200

    forgot to run yarn build:deno.

--- deno/lib/__tests__/pickomit.test.ts
+++ deno/lib/__tests__/pickomit.test.ts
@@ -66,13 +66,13 @@ test("nonstrict inference", () => {
 });
 
 test("nonstrict parsing - pass", () => {
-  const laxfish = fish.nonstrict().pick({ name: true });
+  const laxfish = fish.passthrough().pick({ name: true });
   laxfish.parse({ name: "asdf", whatever: "asdf" });
   laxfish.parse({ name: "asdf", age: 12, nested: {} });
 });
 
 test("nonstrict parsing - fail", () => {
-  const laxfish = fish.nonstrict().pick({ name: true });
+  const laxfish = fish.passthrough().pick({ name: true });
   const bad = () => laxfish.parse({ whatever: "asdf" } as any);
   expect(bad).toThrow();
 });
@@ -85,6 +85,7 @@ test("pick a nonexistent key", () => {
 
   const pickedSchema = schema.pick({
     a: true,
+    // @ts-expect-error
     doesntExist: true,
   });
 
