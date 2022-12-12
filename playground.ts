d95e1cc28bd08be89f6bca3e81ca4db8e5606da1
Colin McDonnell <colinmcd@alum.mit.edu>
2022-12-12 00:22:22 -0800

    Update package.json

--- playground.ts
+++ playground.ts
@@ -1,13 +1,32 @@
 import { Schema, z } from "./src";
 
 async function main() {
-  const schema = z.custom<`${number}px`>(
-    (val) => typeof val === "string" && /^\d+px$/.test(val),
-    {
-      message: "Not a valid px string",
-    }
-  );
+  const dateRangeValidator = (val: {
+    startDate?: Date;
+    endDate?: Date;
+    actualDate: Date;
+  }) => {
+    if (!val.startDate && !val.endDate) return true;
+    if (!val.startDate || !val.endDate) return false;
+    return val.actualDate >= val.startDate && val.actualDate <= val.endDate;
+  };
+  z.object({ asdf: z.string() }, { required_error: "asdf" });
+
+  const schema = z
+    .object({
+      startDate: z.date().optional(),
+      endDate: z.date().optional(),
+      actualDate: z.date(),
+    })
+    .refine(dateRangeValidator);
+
+  z.enum(["asdf"], { required_error: "error mesage" });
   console.log(schema.parse("100px")); // pass
   console.log(schema.parse("100vw")); // fail
 }
 main();
+
+const myErrorMap: z.ZodErrorMap = (val, ctx) => {
+  return { message: "whatever" };
+};
+z.setErrorMap(myErrorMap);
