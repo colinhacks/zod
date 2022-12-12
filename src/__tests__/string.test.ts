72d155325bfa1fc4dc2b3a7ba11ef0bf12b3322f
AnatoleLucet <anatole@hey.com>
2022-12-12 23:13:14 +0100

    fix: #1668 email regex safari compat

--- src/__tests__/string.test.ts
+++ src/__tests__/string.test.ts
@@ -37,8 +37,10 @@ test("email validations", () => {
   expect(() => email.parse("asdf")).toThrow();
   expect(() => email.parse("@lkjasdf.com")).toThrow();
   expect(() => email.parse("asdf@sdf.")).toThrow();
-  // expect(() => email.parse("asdf@asdf.com-")).toThrow();
-  // expect(() => email.parse("asdf@-asdf.com")).toThrow();
+  expect(() => email.parse("asdf@asdf.com-")).toThrow();
+  expect(() => email.parse("asdf@-asdf.com")).toThrow();
+  expect(() => email.parse("asdf@-a(sdf.com")).toThrow();
+  expect(() => email.parse("asdf@-asdf.com(")).toThrow();
 });
 
 test("more email validations", () => {
