39c7c2753156895aecfa58a0f23204cc66480cd1
Colin McDonnell <colinmcd@alum.mit.edu>
2022-12-12 22:24:46 -0800

    Make stalebot a bit more chill

--- playground.ts
+++ playground.ts
@@ -18,4 +18,25 @@ z.object({
   full: `${val.first} ${val.last}`,
 }));
 
-z.number().catch(() => (Array.isArray(e) ? e.length : -1));
+// z.number().catch(() => (Array.isArray(e) ? e.length : -1));
+
+// type keyType = keyof any;
+// // string | number | symbol
+
+// const anyObject = z.record(
+//   z.string().or(z.number()).or(z.symbol()),
+//   z.unknown()
+// );
+
+// const stringifiable = z.record(z.literal(Symbol.toStringTag), z.function());
+// const arg = stringifiable.parse("");
+// arg.toString();
+
+console.log(
+  z
+    .object({
+      first: z.string().catch("first"),
+      second: z.string().catch("second"),
+    })
+    .parse(undefined)
+);
