0aaebaae11b919323e633d5ecf2ab691507b8e11
Karl R <karolrodzinski@gmail.com>
2022-12-17 01:43:52 +0100

    Destructure array to make .map() work properly - ZodArray

--- src/types.ts
+++ src/types.ts
@@ -1689,7 +1689,7 @@ export class ZodArray<
 
     if (ctx.common.async) {
       return Promise.all(
-        (ctx.data as any[]).map((item, i) => {
+        ([...ctx.data] as any[]).map((item, i) => {
           return def.type._parseAsync(
             new ParseInputLazyPath(ctx, item, ctx.path, i)
           );
@@ -1699,7 +1699,7 @@ export class ZodArray<
       });
     }
 
-    const result = (ctx.data as any[]).map((item, i) => {
+    const result = ([...ctx.data] as any[]).map((item, i) => {
       return def.type._parseSync(
         new ParseInputLazyPath(ctx, item, ctx.path, i)
       );
