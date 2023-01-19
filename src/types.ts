d123746b2b13c6b972e08479ad3ad12c00569348
igalklebanov <igalklebanov@gmail.com>
2023-01-19 23:04:14 +0200

    add ZodTemplateLiteral custom errors for unsupported stuff. pt. 2.

--- src/types.ts
+++ src/types.ts
@@ -5753,9 +5753,7 @@ export class ZodTemplateLiteral<Template extends string = ""> extends ZodType<
       return "undefined";
     }
 
-    throw new Error(
-      "Zod error: Unsupported ZodTemplateLiteral argument! Please submit an issue at https://github.com/colinhacks/zod/issues"
-    );
+    throw new ZodTemplateLiteralUnsupportedTypeError();
   }
 
   protected _transformZodStringPartToRegexString(part: ZodString): string {
@@ -5783,13 +5781,6 @@ export class ZodTemplateLiteral<Template extends string = ""> extends ZodType<
         return this._unwrapRegexString(ch.regex.source);
       }
 
-      if (ch.kind === "url") {
-        // FIXME: we use native URL for validation, so cannot regex test in such cases.
-        throw new Error(
-          "Zod error: ZodTemplateLiteral does not support use of ZodString with url check!"
-        );
-      }
-
       if (ch.kind === "uuid") {
         return this._unwrapRegexString(uuidRegex.source);
       }
@@ -5804,6 +5795,11 @@ export class ZodTemplateLiteral<Template extends string = ""> extends ZodType<
         minLength = Math.max(minLength, ch.value);
       } else if (ch.kind === "startsWith") {
         startsWith = ch.value;
+      } else {
+        throw new ZodTemplateLiteralUnsupportedCheckError(
+          ZodFirstPartyTypeKind.ZodString,
+          ch.kind
+        );
       }
     }
 
