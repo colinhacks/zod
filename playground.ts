20df80e6ce2ac27f6e2f26b61cd1fd4c942b152d
Colin McDonnell <colinmcd94@gmail.com>
2023-03-05 01:16:35 -0800

    Add tsc compilation test

--- playground.ts
+++ playground.ts
@@ -1,12 +1,14 @@
 import { z } from "./src";
 
-console.time("parse");
-// for (let i = 0; i < 1000000; i++) {
-//   z.string().safeParse(123);
-// }
-const result = z.string().safeParse(123);
-if (result.success === false) {
-  console.log(result.error);
-  console.log(result.error === result.error);
-}
-console.timeEnd("parse");
+const a = z.object({ a: z.string() });
+const b = z.object({ a, b: z.string() });
+const c = z.object({ a, b, c: z.string() });
+const d = z.object({ a, b, c, d: z.string() });
+const e = z.object({ a, b, c, d, e: z.string() });
+const f = z.object({ a, b, c, d, e, f: z.string() });
+const g = z.object({ a, b, c, d, e, f, g: z.string() });
+const h = z.object({ a, b, c, d, e, f, g, h: z.string() });
+const i = z.object({ a, b, c, d, e, f, g, h, i: z.string() });
+const j = z.object({ a, b, c, d, e, f, g, h, i, j: z.string() });
+
+export const arg = j.parse({});
