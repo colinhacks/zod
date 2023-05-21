75494aca9adf6d4c6878782007491e40daf54078
Colin McDonnell <colinmcd94@gmail.com>
2023-05-21 16:37:49 -0700

    Remove logging

--- playground.ts
+++ playground.ts
@@ -1,2 +1,6 @@
 import { z } from "./src";
 z;
+// const schema = z.string().email();
+const regex =
+  /^([A-Z0-9\!\#\$\%\&\'\*\+\-\/\=\?\^\_\`\{\|\}\~]+\.?)+@[A-Z0-9]([A-Z0-9-]+\.)+[A-Z]{2,}$/i;
+console.log(regex.test("user%example.com@example.org"));
