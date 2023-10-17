5376857d1c32bcc2f9193af5c0355c129ac70697
Rafael Linnankoski <rafaellinnankoski@gmail.com>
2023-10-17 16:49:13 +0900

    Add test for separate guid string validator

--- src/__tests__/string.test.ts
+++ src/__tests__/string.test.ts
@@ -239,6 +239,31 @@ test.each([
   }
 });
 
+test.each([
+  "9491d710-3185-4e06-bea0-6a2f275345e0",
+  "d89e7b01-7598-ed11-9d7a-0022489382fd", // new sequential id
+  "b3ce60f8-e8b9-40f5-1150-172ede56ff74", // Variant 0 - RFC 4122: Reserved, NCS backward compatibility
+  "92e76bf9-28b3-4730-cd7f-cb6bc51f8c09", // Variant 2 - RFC 4122: Reserved, Microsoft Corporation backward compatibility
+  "00000000-0000-0000-0000-000000000000",
+  "ffffffff-ffff-ffff-ffff-ffffffffffff",
+])("guid: %s", (goodGuid: unknown) => {
+  const guid = z.string().guid("custom error");
+  const result = guid.safeParse(goodGuid);
+  expect(result.success).toEqual(true);
+});
+
+test.each(["9491d710-3185-4e06-bea0-6a2f275345e0X"])(
+  "bad guid: %s",
+  (badGuid: unknown) => {
+    const guid = z.string().guid("custom error");
+    const result = guid.safeParse(badGuid);
+    expect(result.success).toEqual(false);
+    if (!result.success) {
+      expect(result.error.issues[0].message).toEqual("custom error");
+    }
+  }
+);
+
 test("cuid", () => {
   const cuid = z.string().cuid();
   cuid.parse("ckopqwooh000001la8mbi2im9");
