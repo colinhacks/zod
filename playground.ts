41c82f83415c0d659beb7000953fa91e18e8f237
zce <w@zce.me>
2023-12-07 15:11:46 +0800

    feat: metadata

--- playground.ts
+++ playground.ts
@@ -1,65 +1,32 @@
 import { z } from "./src";
+import * as mod from "node:module";
+z;
 
-declare module './src' {
-  interface ZodMeta {
-    foo: string
+// @ts-ignore
+// const arg = await import("./index");
+// arg;
+// require.resolve(arg);
+
+load: {
+}
+interface A<T> {
+  name: T;
+  children: any;
+  render(): ReadableStream | string;
+}
+function Hyper<T>(arg: T) {
+  return function () {} as any as {
+    prototype: A<T>;
+    new (): A<T>;
+  };
+}
+
+export default class extends Hyper({ name: 1234 }) {
+  render() {
+    // this.name.name;
+    return `asdf ${this.children} asdf`;
   }
 }
 
-const basicSchema = z.string().transform((val, ctx) => {
-  console.log(ctx.meta.foo)
-  return val.toUpperCase()
-})
-
-const res1 = basicSchema.parse('zce', { meta: { foo: 'bar' } })
-console.log(res1)
-
-const objSchema = z
-  .object({
-    name: z.string().transform((val, ctx) => {
-      console.log(ctx.meta.foo)
-      console.log(ctx.path)
-      return val.toUpperCase()
-    }),
-    age: z.number().superRefine((val, ctx) => {
-      console.log(ctx.meta.foo)
-      console.log(ctx.path)
-      return val > 0
-    })
-  })
-
-const res2 = objSchema.parse({ name: 'John', age: 30 }, { meta: { foo: 'bar' } })
-console.log(res2)
-
-// import { z } from "./src";
-// import * as mod from "node:module";
-// z;
-
-// // @ts-ignore
-// // const arg = await import("./index");
-// // arg;
-// // require.resolve(arg);
-
-// load: {
-// }
-// interface A<T> {
-//   name: T;
-//   children: any;
-//   render(): ReadableStream | string;
-// }
-// function Hyper<T>(arg: T) {
-//   return function () {} as any as {
-//     prototype: A<T>;
-//     new (): A<T>;
-//   };
-// }
-
-// export default class extends Hyper({ name: 1234 }) {
-//   render() {
-//     // this.name.name;
-//     return `asdf ${this.children} asdf`;
-//   }
-// }
-
-// const a = new Arg();
-// a.name;
+const a = new Arg();
+a.name;
