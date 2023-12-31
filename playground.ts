05d77529921aa103ec39ccc08adf2f74be57c0c0
Tilak Thapa <iamjrtilak@gmail.com>
2023-12-31 09:58:22 +0545

    fixes: add sample schema validation to play

--- playground.ts
+++ playground.ts
@@ -1,32 +1,54 @@
-import { z } from "./src";
-import * as mod from "node:module";
-z;
+/**
+ * Description: This file is used to test the zod library after any changes are made in development mode.
+ * Donot push this file to the repository. Keep is as it is while pushing to the repository.
+ * To run this file, run the command "npm run play" in the terminal.
+ */
 
-// @ts-ignore
-// const arg = await import("./index");
-// arg;
-// require.resolve(arg);
+import { z } from "./src"; //donot remove this line
 
-load: {
-}
-interface A<T> {
-  name: T;
-  children: any;
-  render(): ReadableStream | string;
-}
-function Hyper<T>(arg: T) {
-  return function () {} as any as {
-    prototype: A<T>;
-    new (): A<T>;
-  };
+//play with zod here
+
+//sample user schema
+const UserSchema = z.object({
+  username: z.string().min(3).max(10).startsWith("user_"),
+  age: z.number().int().positive(),
+  name: z.string().min(5).max(20),
+  address: z.string().min(10).max(50).optional(),
+});
+
+const user1 = {
+  username: "user_john",
+  age: 20,
+  name: "John Doe",
+  address: "Some address",
+};
+
+const user2 = {
+  username: "miles",
+  age: 20,
+  name: "Miles",
+  address: "Some address 2",
+};
+
+console.log("\n\n\n");
+
+// user1 is valid
+try {
+  UserSchema.parse(user1);
+  console.log("user1 is valid");
+} catch (err) {
+  console.log("user1 is invalid");
 }
 
-export default class extends Hyper({ name: 1234 }) {
-  render() {
-    // this.name.name;
-    return `asdf ${this.children} asdf`;
-  }
+// user2 is invalid
+try {
+  UserSchema.parse(user2);
+  console.log("user2 is valid");
+} catch (err) {
+  console.log("user2 is invalid");
 }
+console.log("\n\n\n");
 
-const a = new Arg();
-a.name;
+// types of user
+export type User = z.infer<typeof UserSchema>;
+
