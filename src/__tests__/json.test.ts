7c48c83f9cd907f55bf5b5633aedc2a8a6d2c972
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-01-08 13:38:19 -0500

    use parseAsync + await expect(...).rejects

--- src/__tests__/json.test.ts
+++ src/__tests__/json.test.ts
@@ -3,7 +3,7 @@ import { expect, test } from "@jest/globals";
 
 import * as z from "../index";
 
-test("parse string to json", () => {
+test("parse string to json", async () => {
   const Env = z.object({
     myJsonConfig: z.string().json(z.object({ foo: z.number() })),
     someOtherValue: z.string(),
@@ -19,58 +19,50 @@ test("parse string to json", () => {
     someOtherValue: "abc",
   });
 
-  expect(() =>
-    Env.parse({
+  await expect(
+    Env.parseAsync({
       myJsonConfig: '{"foo": "not a number!"}',
       someOtherValue: null,
     })
-  ).toThrow(
-    JSON.stringify(
-      [
-        {
-          code: "invalid_type",
-          expected: "number",
-          received: "string",
-          path: ["myJsonConfig", "foo"],
-          message: "Expected number, received string",
-        },
-        {
-          code: "invalid_type",
-          expected: "string",
-          received: "null",
-          path: ["someOtherValue"],
-          message: "Expected string, received null",
-        },
-      ],
-      null,
-      2
-    )
-  );
+  ).rejects.toMatchObject({
+    issues: [
+      {
+        code: "invalid_type",
+        expected: "string",
+        received: "null",
+        path: ["someOtherValue"],
+        message: "Expected string, received null",
+      },
+      {
+        code: "invalid_type",
+        expected: "number",
+        received: "string",
+        path: ["myJsonConfig", "foo"],
+        message: "Expected number, received string",
+      },
+    ],
+  });
 
-  expect(() =>
-    Env.parse({
+  await expect(
+    Env.parseAsync({
       myJsonConfig: "This is not valid json",
       someOtherValue: null,
     })
-  ).toThrow(
-    JSON.stringify(
-      [
-        {
-          code: "invalid_string",
-          validation: "json",
-          message: "Unexpected token T in JSON at position 0",
-          path: ["myJsonConfig"],
-        },
-        {
-          code: "invalid_type",
-          expected: "string",
-          received: "null",
-          path: ["someOtherValue"],
-          message: "Expected string, received null",
-        },
-      ],
-      null,
-      2
-    )
-  );
+  ).rejects.toMatchObject({
+    issues: [
+      {
+        code: "invalid_type",
+        expected: "string",
+        received: "null",
+        path: ["someOtherValue"],
+        message: "Expected string, received null",
+      },
+      {
+        code: "invalid_string",
+        validation: "json",
+        message: "Unexpected token T in JSON at position 0",
+        path: ["myJsonConfig"],
+      },
+    ],
+  });
 });
