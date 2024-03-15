408cf8f06078e4bde40d9875798803cfc9b48e8f
Colin McDonnell <colinmcd94@gmail.com>
2024-03-14 18:37:34 -0700

    Switch back to yarn

--- playground.ts
+++ playground.ts
@@ -2,8 +2,4 @@ import { z } from "./src";
 
 z;
 
-function dataObj<T extends z.ZodTypeAny>(schema: T) {
-  return z.object({ data: schema as T }).transform((a: T['_output']) => a.data);
-}
-
-const arg= dataObj(z.string()).parse({ data: "asdf" });
+z.string().parse(2);
