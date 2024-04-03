761ecc623bf316e4c1d63eb2d89057d7d6cb86e9
Charles Catta <charles.catta@botpress.com>
2024-04-03 15:45:37 -0400

    Remove Deno

--- deno/lib/external.ts
+++
@@ -1,6 +0,0 @@
-export * from "./errors.ts";
-export * from "./helpers/parseUtil.ts";
-export * from "./helpers/typeAliases.ts";
-export * from "./helpers/util.ts";
-export * from "./types.ts";
-export * from "./ZodError.ts";
--- deno/lib/index.ts
+++
@@ -1,4 +0,0 @@
-import * as z from "./external.ts";
-export * from "./external.ts";
-export { z };
-export default z;
