6f56869687b720d7266b9101631a68165c1199b9
Tim <hello@timsmart.co>
2024-04-28 08:41:04 +1200

    suggestions for effect plugin (#3449)

--- plugin/effect/src/index.ts
+++ plugin/effect/src/index.ts
@@ -1,21 +1,20 @@
-import { Effect } from "effect";
+import * as Effect from "effect/Effect";
 import * as z from "zod";
 
 function zodEffect(this: z.ZodType, data: unknown, params?: any) {
-  return Effect.tryPromise({
-    try: async () => this.parseAsync(data, params),
-    catch(error) {
-      return error as z.ZodError;
-    },
-  });
+  return Effect.flatMap(
+    Effect.promise(() => this.safeParseAsync(data, params)),
+    (result) =>
+      result.success ? Effect.succeed(result.data) : Effect.fail(result.error)
+  );
 }
 
 function zodEffectSync(this: z.ZodType, data: unknown, params?: any) {
-  return Effect.try({
-    try: () => this.parse(data, params),
-    catch(error) {
-      return error as z.ZodError;
-    },
+  return Effect.suspend(() => {
+    const result = this.safeParse(data, params);
+    return result.success
+      ? Effect.succeed(result.data)
+      : Effect.fail(result.error);
   });
 }
 
@@ -31,10 +30,10 @@ declare module "zod" {
   interface ZodType {
     effect(
       ...args: Parameters<z.ZodType["parseAsync"]>
-    ): Effect.Effect<this["_output"], z.ZodError<this["_output"]>, never>;
+    ): Effect.Effect<this["_output"], z.ZodError<this["_output"]>>;
     effectSync(
       ...args: Parameters<z.ZodType["parse"]>
-    ): Effect.Effect<this["_output"], z.ZodError<this["_output"]>, never>;
+    ): Effect.Effect<this["_output"], z.ZodError<this["_output"]>>;
   }
 
   interface ZodError {
