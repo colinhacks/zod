5f86de6682d121dde1ab1d20616edfceb4743003
Colin McDonnell <colinmcd94@gmail.com>
2024-05-10 10:57:40 -0700

    Add experiments

---
+++ experiments/oops.ts
@@ -0,0 +1,80 @@
+// a less object-oriented approach
+
+interface ZodDef {
+  kind: string;
+}
+
+abstract class ZodType implements ZodDef {
+  kind: string;
+
+  "~metadata"?: unknown;
+  "~output": unknown;
+  "~input": unknown;
+  "~context": unknown;
+  abstract "~parse"(x: unknown): this["~output"];
+
+  shortcircuits: Set<Primitive> = new Set();
+
+  meta(): this["~metadata"];
+  meta<M>(meta: M): $meta<this, M>;
+  meta<M>(meta?: M) {
+    if (meta === undefined) return this["~metadata"];
+    return { ...this, "~metadata": meta };
+  }
+
+  optional(): $optional<this> {
+    return {
+      ...this,
+      shortcircuits: new Set([...this.shortcircuits, undefined]),
+    };
+  }
+}
+
+type Primitive = string | number | boolean | bigint | symbol | undefined | null;
+type PrimitiveTypes =
+  | "string"
+  | "number"
+  | "boolean"
+  | "bigint"
+  | "symbol"
+  | "undefined"
+  | "null";
+
+abstract class ZodPrimitive extends ZodType {
+  kind: "primitive" = "primitive";
+  types: { [k in PrimitiveTypes]?: true | false };
+  "~output": Primitive;
+  "~input": Primitive;
+}
+
+class ZodString extends ZodPrimitive {
+  types: { string: true } = { string: true };
+  "~output": string;
+  "~input": string;
+  "~parse"(x: unknown): string {
+    if (typeof x === "string") return x;
+    throw new Error();
+  }
+}
+
+type $meta<T extends ZodType, M> = T & { "~metadata": M; "~inner": T };
+const $optional = Symbol("optional");
+type $optional<T extends ZodType> = Omit<T, "~output" | "~input"> & {
+  "~output": T["~output"] | typeof $optional;
+  "~input": T["~input"] | typeof $optional;
+};
+
+type OmitVirtuals<T> = {
+  [k in keyof T as k extends `~${string}` ? never : k]: T[k];
+};
+
+function makeString(): ZodString {
+  return new ZodString();
+}
+
+const schema = makeString().meta({ hello: "world" }).optional();
+schema.meta();
+schema["~output"];
+console.log(schema["~parse"]("asdfasdf"));
+
+// z.string().mutate(val => val.toLowerCase());
