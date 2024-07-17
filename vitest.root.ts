6d5916cb3f333632545b808459ffa1bd634c2678
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-07-17 15:49:32 -0400

    separate typecheck from runtime testing

--- vitest.root.ts
+++ vitest.root.ts
@@ -2,13 +2,6 @@ import { type UserConfig, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["**/*.test.ts"],
-    isolate: false,
     watch: false,
-    typecheck: {
-      enabled: true,
-      ignoreSourceErrors: true,
-      include: ["**/*.test.ts"],
-    },
   },
 }) as UserConfig;
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,1 +1,22 @@
-export default ["packages/*"] as [string];
+import { defineWorkspace } from "vitest/config";
+
+export default defineWorkspace([
+  "packages/*",
+  {
+    extends: "./vitest.root.ts",
+    test: {
+      include: ["**/*.test.ts"],
+      isolate: false,
+    },
+  },
+  {
+    extends: "./vitest.root.ts",
+    test: {
+      typecheck: {
+        enabled: true,
+        ignoreSourceErrors: true,
+        include: ["**/*.test.ts"],
+      },
+    },
+  },
+]) as ReturnType<typeof defineWorkspace>;
