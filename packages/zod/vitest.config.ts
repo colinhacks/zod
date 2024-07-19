768096bd8dad89ed1c956b8f8cc7d7aa79ce030f
Colin McDonnell <colinmcd94@gmail.com>
2024-07-18 17:02:08 -0700

    Fix vitest issue

--- packages/zod/vitest.config.ts
+++ packages/zod/vitest.config.ts
@@ -1,5 +1,4 @@
 import { defineProject, mergeConfig } from "vitest/config";
-// @ts-ignore
 import rootConfig from "../../vitest.root.mjs";
 
 export default mergeConfig(rootConfig, defineProject({})) as object;
