ad0a7e1411294b4b804e2cc827bb53af1cbf00e0
Klaus L. Hougesen <klaus.hougesen@gmail.com>
2024-12-04 13:04:50 +0100

    Updated base64 in types.ts to use chunk processing

--- src/types.ts
+++ src/types.ts
@@ -935,7 +935,7 @@ export class ZodString extends ZodType<string, ZodStringDef, string> {
           status.dirty();
         }
       } else if (check.kind === "base64") {
-        if (!base64Regex.test(input.data)) {
+        if(!processInChunks(input.data, 2048, 4, (chunk)=>base64Regex(chunk))) {
           ctx = this._getOrReturnCtx(input, ctx);
           addIssueToContext(ctx, {
             validation: "base64",
