1d0a4b95300a2c470b175ed4524fe3cf04ef9b19
Christoffer <hi@kodemon.net>
2024-12-10 02:26:39 +0100

    fix: bigint coerce crash (#3822)
    * fix: cast invalid bigint inputs to undefined
    
    * fix: cast invalid bigint values to undefined
    
    * refactor: return invalid type on coerce error

--- src/types.ts
+++ src/types.ts
@@ -1551,17 +1551,15 @@ export interface ZodBigIntDef extends ZodTypeDef {
 export class ZodBigInt extends ZodType<bigint, ZodBigIntDef, bigint> {
   _parse(input: ParseInput): ParseReturnType<bigint> {
     if (this._def.coerce) {
-      input.data = BigInt(input.data);
+      try {
+        input.data = BigInt(input.data);
+      } catch {
+        return this._getInvalidInput(input);
+      }
     }
     const parsedType = this._getType(input);
     if (parsedType !== ZodParsedType.bigint) {
-      const ctx = this._getOrReturnCtx(input);
-      addIssueToContext(ctx, {
-        code: ZodIssueCode.invalid_type,
-        expected: ZodParsedType.bigint,
-        received: ctx.parsedType,
-      });
-      return INVALID;
+      return this._getInvalidInput(input);
     }
 
     let ctx: undefined | ParseContext = undefined;
@@ -1614,6 +1612,16 @@ export class ZodBigInt extends ZodType<bigint, ZodBigIntDef, bigint> {
     }
 
     return { status: status.value, value: input.data };
+  }
+
+  _getInvalidInput(input: ParseInput) {
+    const ctx = this._getOrReturnCtx(input);
+    addIssueToContext(ctx, {
+      code: ZodIssueCode.invalid_type,
+      expected: ZodParsedType.bigint,
+      received: ctx.parsedType,
+    });
+    return INVALID;
   }
 
   static create = (
