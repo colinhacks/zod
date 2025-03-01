a5d98effb88d533e145244bcf44cac0d23225508
Colin McDonnell <colinmcd94@gmail.com>
2025-03-01 01:28:58 -0800

    Remove effect plugin

--- packages/effect-plugin/vitest.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineProject, mergeConfig } from "vitest/config";
-import rootConfig from "../../vitest.root.mjs";
-
-const config: Record<string, any> = mergeConfig(rootConfig, defineProject({}));
-export default config;
