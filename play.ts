db68bc92366e0c6e66531acc987de83336ad7884
Colin McDonnell <colinmcd94@gmail.com>
2025-04-08 14:51:41 -0700

    Final versioning

--- play.ts
+++ play.ts
@@ -2,18 +2,24 @@ import * as z from "zod";
 
 z;
 
-const schema = z.strictInterface({
-  username: z.string(),
-  favoriteNumbers: z.array(z.number()),
-});
-const result = schema.safeParse({
-  username: 1234,
-  favoriteNumbers: [1234, "4567"],
-  extraKey: "extraValue",
-});
-console.log(result.error!.issues);
+// const schema = z.strictInterface({
+//   username: z.string(),
+//   favoriteNumbers: z.array(z.number()),
+// });
+// const result = schema.safeParse({
+//   username: 1234,
+//   favoriteNumbers: [1234, "4567"],
+//   extraKey: "extraValue",
+// });
+// console.log(result.error!.issues);
 
-console.dir(z.treeifyError(result.error!), { depth: null });
-console.log(z.prettifyError(result.error!));
-console.dir(z.flattenError(result.error!), { depth: null });
-console.dir(z.formatError(result.error!), { depth: null });
+// console.dir(z.treeifyError(result.error!), { depth: null });
+// console.log(z.prettifyError(result.error!));
+// console.dir(z.flattenError(result.error!), { depth: null });
+// console.dir(z.formatError(result.error!), { depth: null });
+
+// z.optional(z.string());
+// z.union([z.string(), z.number()]);
+// z.string().check(z.refine((val) => val.includes("@")));
+// z.array(z.number()).check(z.minLength(5), z.maxLength(10));
+// z.extend(z.object({ name: z.string() }), { age: z.number() });
