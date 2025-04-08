3a153fcf3a8fa12042cbbe6042e4970fb0d518f7
Colin McDonnell <colinmcd94@gmail.com>
2025-04-08 14:57:19 -0700

    Pub

--- scripts/prepublish.ts
+++ scripts/prepublish.ts
@@ -1,12 +1,12 @@
 import { readFileSync } from "node:fs";
 
-// read package.json for zod and zod-core
+// read package.json for zod and @zod/mini
 const zodPackageJson = JSON.parse(readFileSync("packages/zod/package.json", "utf-8"));
-const zodCorePackageJson = JSON.parse(readFileSync("packages/zod-core/package.json", "utf-8"));
+const zodMiniPackageJson = JSON.parse(readFileSync("packages/mini/package.json", "utf-8"));
 
 // check that versions are the same
-if (zodPackageJson.version !== zodCorePackageJson.version) {
+if (zodPackageJson.version !== zodMiniPackageJson.version) {
   throw new Error(
-    `zod and zod-core versions do not match: ${zodPackageJson.version} !== ${zodCorePackageJson.version}`
+    `zod and @zod/mini versions do not match: ${zodPackageJson.version} !== ${zodMiniPackageJson.version}`
   );
 }
