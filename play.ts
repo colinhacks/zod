2792efb1b9ac0f4a2b0ec775aedb156bc91b80e1
Colin McDonnell <colinmcd94@gmail.com>
2025-04-09 16:20:54 -0700

    Update core

--- play.ts
+++ play.ts
@@ -1,31 +1,49 @@
-import * as z from "zod";
+import * as z from "@zod/core";
 
 z;
 
-// const schema = z.strictInterface({
-//   username: z.string(),
-//   favoriteNumbers: z.array(z.number()),
-// });
-// const result = schema.safeParse({
-//   username: 1234,
-//   favoriteNumbers: [1234, "4567"],
-//   extraKey: "extraValue",
-// });
-// console.log(result.error!.issues);
+// interface MyStringSchema extends z.$ZodString {}
+// class MyStringSchema {
+//   constructor() {
+//     z.$ZodString.init(this, {
+//       type: "string",
+//     });
+//   }
 
-// console.dir(z.treeifyError(result.error!), { depth: null });
-// console.log(z.prettifyError(result.error!));
-// console.dir(z.flattenError(result.error!), { depth: null });
-// console.dir(z.formatError(result.error!), { depth: null });
+//   handle(value: string) {
+//     const result = this._zod.parse({ issues: [], value }, {});
+//     if (result instanceof Promise) {
+//       throw new Error("Promise not supported");
+//     }
+//     result.issues;
+//   }
+// }
 
-// z.optional(z.string());
-// z.union([z.string(), z.number()]);
-// z.string().check(z.refine((val) => val.includes("@")));
-// z.array(z.number()).check(z.minLength(5), z.maxLength(10));
-// z.extend(z.object({ name: z.string() }), { age: z.number() });
+// const myStringSchema = new MyStringSchema();
+// const result = myStringSchema.handle("hello");
+// console.log(result);
 
-// const schema = z.coerce.string();
-// type schemaInput = z.input<typeof schema>;
-// type schemaOutput = z.output<typeof schema>;
+const check = {} as z.$ZodChecks;
+const def = check._zod.def;
 
-z.string();
+switch (def.check) {
+  case "less_than":
+  case "greater_than":
+  // ...
+  case "string_format":
+    {
+      const formatCheck = check as z.$ZodStringFormatChecks;
+      const formatCheckDef = formatCheck._zod.def;
+
+      switch (formatCheckDef.format) {
+        case "email":
+          // ...
+          break;
+        case "url":
+          // ...
+          break;
+        // etc
+      }
+    }
+    break;
+}
