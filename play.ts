f20e32113734637f22e96c176254cd8f3ef6117b
Colin McDonnell <colinmcd94@gmail.com>
2025-04-09 21:44:09 -0700

    Clean up mini docs

--- play.ts
+++ play.ts
@@ -1,3 +1,15 @@
-import * as z from "zod";
+// import * as z from "zod";
 
-z;
+import * as z from "@zod/mini";
+
+z.array(z.number()).check(
+  z.minLength(5),
+  z.maxLength(10),
+  z.refine((arr) => arr.includes(5))
+);
+// import * as z from "zod";
+
+// z.array(z.number())
+//   .min(5)
+//   .max(10)
+//   .refine((arr) => arr.includes(5));
