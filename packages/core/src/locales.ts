a5c620fd117a60ff1e85bd15322c6be8942d60b0
Henri Södergård <henri.sodergard@taitounited.fi>
2025-04-16 17:03:08 +0300

    chore: add export and docs

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import fi from "./locales/fi.js";
 
-export { az, es, en };
+export { az, es, en, fi };
