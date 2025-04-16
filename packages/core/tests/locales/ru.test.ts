3f74f864fb5a12d6e794ea2ada35db119447172c
VladSez <16666234+VladSez@users.noreply.github.com>
2025-04-16 22:25:37 +0200

    fix: tests

--- packages/core/tests/locales/ru.test.ts
+++ packages/core/tests/locales/ru.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import ru from "../ru.js";
+import ru from "../../src/locales/ru.js";
 
 describe("Russian localization", () => {
   const { localeError } = ru();
