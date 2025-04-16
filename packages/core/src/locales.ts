0eda273418469e69a4b8e4fd89b4911328e72fcf
Davide Lombardi <davidelombardi130p@gmail.com>
2025-04-16 17:22:56 +0200

    feat: added italian language

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import it from "./locales/it.js";
 
-export { az, es, en };
+export { az, es, en, it };
