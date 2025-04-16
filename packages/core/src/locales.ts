e04ca4c2f33902234e4c83ed2dc4673691f46f9f
VladSez <16666234+VladSez@users.noreply.github.com>
2025-04-16 19:55:49 +0200

    feat(locale): add belarusian language

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
+import be from "./locales/be.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
 
-export { az, es, en };
+export { az, be, es, en };
