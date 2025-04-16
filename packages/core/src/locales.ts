7c2b1dbce251514305090200d503e0771e4d627f
Bartosz Szar <szarbartosz@gmail.com>
2025-04-16 15:37:24 +0200

    Languages: add polish language

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import pl from './locales/pl.js';
 
-export { az, es, en };
+export { az, es, en, pl };
