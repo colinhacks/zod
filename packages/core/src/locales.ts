86bb27b72ff267a1d8c49ee37cd297a1ca84b21e
Shachar Zidon <theonlytails@theonlytails.com>
2025-04-16 15:36:48 +0300

    Update locales.ts

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import he from "./locales/he.js";
 
-export { az, es, en };
+export { az, es, en, he };
