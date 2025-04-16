7c828aa6ea0988ca3009dc705a59ab4a23c4d652
krisna <krisna.w2010@gmail.com>
2025-04-16 23:35:09 +0800

    Languages: add Bahasa Indonesia language

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import id from "./locales/id.js";
 
-export { az, es, en };
+export { az, es, en, id };
