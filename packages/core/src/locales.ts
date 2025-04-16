57420d8686e643a380496486c5de1864783eb07e
MrBarracda <1dima9999@gmail.com>
2025-04-16 15:51:53 +0200

    locale: add ukrainian language

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import ua from "./locales/ua.js";
 
-export { az, es, en };
+export { az, es, en, ua };
