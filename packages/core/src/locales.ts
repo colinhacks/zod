76c440489ff84321cbe09fe40eb17661b83f9f2e
Kio Yoshimatsu <69349613+yoshimatsu567@users.noreply.github.com>
2025-04-16 12:48:56 +0900

    Add Japanese locale

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import ja from "./locales/ja.js";
 
-export { az, es, en };
+export { az, es, en, ja };
