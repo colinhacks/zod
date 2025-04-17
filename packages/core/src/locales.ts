40041cb2bb0aef447edbe8a580ae93ce703af2e4
AsharibAli <asharibarain4@gmail.com>
2025-04-17 14:18:33 +0500

    feat(locale): add urdu language

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import ur from "./locales/ur.js";
 
-export { az, es, en };
+export { az, es, en, ur };
