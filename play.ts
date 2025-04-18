c12f9082f7ddf0cb3bb9696bba6720c346690e3e
Colin McDonnell <colinmcd94@gmail.com>
2025-04-18 14:53:14 -0700

    Clarify error docs

--- play.ts
+++ play.ts
@@ -1,17 +1,2 @@
-import * as z from "@zod/mini";
-
-console.log(z.toJSONSchema(z.literal(undefined), { unrepresentable: "any" }));
-
-const A = z.partialRecord(z.enum(["a", "b"]), z.number());
-type A = z.infer<typeof A>;
-
-z.string("Bad!");
-z.string().check(z.minLength(5, "Too short!"));
-z.uuid("Bad UUID!");
-z.iso.date("Bad date!");
-z.array(z.string(), "Bad array!");
-z.array(z.string()).check(z.minLength(5, "Too few items!"));
-z.set(z.string(), "Bad set!");
-z.array(z.string(), "Bad array!");
-z.set(z.string(), "Bad set!");
-z.array(z.string(), "Bad array!");
+import * as z from "zod";
+z;
