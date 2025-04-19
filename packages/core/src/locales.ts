1e5572b1ebb4cf20c325306b31041429e9c2adfe
Sasivarnan R <sasivarnan@hotmail.com>
2025-04-19 18:58:19 +0530

    feat(lang): Added initial `Tamil` locale

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import ta from "./locales/ta.js";
 
-export { az, es, en };
+export { az, es, en, ta };
