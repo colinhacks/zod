0259ca59c94819c971877295224a450e53c09c8d
Colin McDonnell <colinmcd94@gmail.com>
2025-04-30 17:25:38 -0700

    z._default

--- play.ts
+++ play.ts
@@ -1,5 +1,11 @@
-import * as z from "zod";
+import * as z from "@zod/mini";
 
 // console.log(z.int().min(0).parse(234));
 
 console.log(z.number().safe().parse(123.0));
+
+z.number().safe();
+
+z.core.$ZodCustom;
+
+z._default;
