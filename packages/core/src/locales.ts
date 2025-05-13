82e1519a91628873e811dc4d64e65870f0b30f15
Bartosz Szar <szarbartosz@gmail.com>
2025-05-13 21:51:42 +0200

    Languages: add Polish 🇵🇱 language (#4184)
    * Languages: add polish language
    
    * Fix: apply formatting
    
    * Tweak
    
    * Fix: add missing fallbacks
    
    * Update packages/core/src/locales/pl.ts
    
    Co-authored-by: Marcel <51132547+marceleq27@users.noreply.github.com>
    
    * Update packages/core/src/locales/pl.ts
    
    Co-authored-by: Marcel <51132547+marceleq27@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Colin McDonnell <colinmcd94@gmail.com>
    Co-authored-by: Marcel <51132547+marceleq27@users.noreply.github.com>

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -2,9 +2,10 @@ import ar from "./locales/ar.js";
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import pl from "./locales/pl.js";
 import he from "./locales/he.js";
 import fr from "./locales/fr.js";
 import ja from "./locales/ja.js";
 import pt from "./locales/pt.js";
 
-export { ar, az, es, en, he, pt, ja, fr };
+export { ar, az, es, en, he, pt, ja, fr, pl };
