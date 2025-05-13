a722359bbda0f6b25978b5b002a617aa17b64b99
Sasivarnan R <sasivarnan@users.noreply.github.com>
2025-05-14 01:57:05 +0530

    feat(lang): Added initial `Tamil` locale (#4237)
    Co-authored-by: Colin McDonnell <colinmcd94@gmail.com>

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -4,6 +4,7 @@ import be from "./locales/be.js";
 import cs from "./locales/cs.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import ta from "./locales/ta.js";
 import sl from "./locales/sl.js";
 import th from "./locales/th.js";
 import no from "./locales/no.js";
@@ -29,5 +30,5 @@ import fr from "./locales/fr.js";
 import ja from "./locales/ja.js";
 import pt from "./locales/pt.js";
 
-export { ar, az, be, cs, es, en, fa, fi, fr, frCA, he, hu, id, it, ja, ko, no, ota, pl, pt, ru, sl, th, tr, ua, ur, vi, zhCN, zhTW };
+export { ar, az, be, cs, es, en, fa, fi, fr, frCA, he, hu, id, it, ja, ko, no, ota, pl, pt, ru, sl, ta, th, tr, ua, ur, vi, zhCN, zhTW };
 
