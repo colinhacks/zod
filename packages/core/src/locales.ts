c2ca0dd066f6c5ee3fec9b9147ed3e9a8d68e7a3
Helmer <11061182+helmerdx@users.noreply.github.com>
2025-05-13 13:11:12 -0600

    Languages: Add french language (#4172)
    * Adding french and exporting it
    
    * Adding french in docs
    
    * Update fr.ts
    
    Co-authored-by: Florian Lefebvre <contact@florian-lefebvre.dev>
    
    * Translating datetime
    
    * Tweak
    
    * Update elements
    
    * Update packages/core/src/locales/fr.ts
    
    Co-authored-by: Fabrice Cipolla <fabricecipolla@gmail.com>
    
    ---------
    
    Co-authored-by: Helmer <11061182+helmerdavila@users.noreply.github.com>
    Co-authored-by: Florian Lefebvre <contact@florian-lefebvre.dev>
    Co-authored-by: Colin McDonnell <colinmcd94@gmail.com>
    Co-authored-by: Fabrice Cipolla <fabricecipolla@gmail.com>

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,7 +1,8 @@
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import fr from "./locales/fr.js";
 import ja from "./locales/ja.js";
 import pt from "./locales/pt.js";
 
-export { az, es, en, pt, ja };
+export { az, es, en, pt, ja, fr };
