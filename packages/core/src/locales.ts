0c4b731479a219814f9797d2b79bc25ce69fbc69
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2025-05-13 21:53:07 +0200

    Languages: add Ukrainian language (#4185)
    * locale: add ukrainian language
    
    * Tweak
    
    ---------
    
    Co-authored-by: Colin McDonnell <colinmcd94@gmail.com>

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -2,10 +2,11 @@ import ar from "./locales/ar.js";
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import ua from "./locales/ua.js";
 import pl from "./locales/pl.js";
 import he from "./locales/he.js";
 import fr from "./locales/fr.js";
 import ja from "./locales/ja.js";
 import pt from "./locales/pt.js";
 
-export { ar, az, es, en, he, pt, ja, fr, pl };
+export { ar, az, es, en, he, pt, ja, fr, pl, ua };
