07cee6db39eb8fee57938effcd60e0db076af6d7
Abdalrhman Almarakeby <contact@abdalrhman.me>
2025-05-13 22:16:57 +0300

    Languages: Add Arabic language (#4176)
    * feat(lang): Add Arabic locale support
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * remove period
    
    * fix: add space after comparison operator
    
    * feat(lang): Add Arabic locale support
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * Update packages/core/src/locales/ar.ts
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    
    * remove period
    
    * fix: add space after comparison operator
    
    * Tweak
    
    * Updated ar.ts with new edits discussed in #4176
    
    * Update packages/core/src/locales/ar.ts
    
    * Update packages/core/src/locales/ar.ts
    
    * Update packages/core/src/locales/ar.ts
    
    * chore: format
    
    * fix: update translation for template_literal
    
    * fix: correct translation for invalid input error message
    
    * fix: spacing in error messages
    
    * fix: show correct prefix
    
    * fix: remove unused template literal
    
    * Update error-customization.mdx
    
    ---------
    
    Co-authored-by: Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
    Co-authored-by: Colin McDonnell <colinmcd94@gmail.com>
    Co-authored-by: Hosam Hamdy <hosam5553@gmail.com>

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,3 +1,4 @@
+import ar from "./locales/ar.js";
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
@@ -5,4 +6,4 @@ import fr from "./locales/fr.js";
 import ja from "./locales/ja.js";
 import pt from "./locales/pt.js";
 
-export { az, es, en, pt, ja, fr };
+export { ar, az, es, en, pt, ja, fr };
