cae1ed3a2dc2203494069fe4534881d8a88d6bfe
Shachar Zidon <theonlytails@theonlytails.com>
2025-05-13 22:18:03 +0300

    Languages: Add Hebrew language (#4183)
    * Create he.ts
    
    * Update locales.ts
    
    * Update error-customization.mdx
    
    * Tweak
    
    ---------
    
    Co-authored-by: Colin McDonnell <colinmcd94@gmail.com>

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -2,8 +2,9 @@ import ar from "./locales/ar.js";
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import he from "./locales/he.js";
 import fr from "./locales/fr.js";
 import ja from "./locales/ja.js";
 import pt from "./locales/pt.js";
 
-export { ar, az, es, en, pt, ja, fr };
+export { ar, az, es, en, he, pt, ja, fr };
