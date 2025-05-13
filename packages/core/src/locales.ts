840b8054eeae2e4857fa41c56d07edc7d2a6b77e
Helmer <11061182+helmerdx@users.noreply.github.com>
2025-05-13 13:10:07 -0600

    Languages: Add Portuguese language (#4171)
    * Adding portuguese translations
    
    * Adding portuguese
    
    * Adding language to docs
    
    * Adding spanish language in docs
    
    * Tweak
    
    * Updating references from string to texto
    
    * Updating faixa to faixa de
    
    * Updating from elementos to itens
    
    * Mirroring validation from original file
    
    * Updating regex to padrao
    
    * Updating entrada by tipo
    
    * Replacing entrada by campo
    
    ---------
    
    Co-authored-by: Helmer <11061182+helmerdavila@users.noreply.github.com>
    Co-authored-by: Colin McDonnell <colinmcd94@gmail.com>

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -2,5 +2,6 @@ import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
 import ja from "./locales/ja.js";
+import pt from "./locales/pt.js";
 
-export { az, es, en, ja };
+export { az, es, en, pt, ja };
