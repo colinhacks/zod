caf6509c3e28198a2db9cd6e32b6f6c4c13eeb98
GrahamQuan <33834833+GrahamQuan@users.noreply.github.com>
2025-05-14 03:58:16 +0800

    Add Simplified Chinese (#4189)
    * feat(lang): add Simplified Chinese
    
    * fix: locale
    
    * fix: space format
    
    * Tweak
    
    ---------
    
    Co-authored-by: Colin McDonnell <colinmcd94@gmail.com>

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -2,6 +2,7 @@ import ar from "./locales/ar.js";
 import az from "./locales/az.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
+import zhCN from "./locales/zh-CN.js";
 import hu from "./locales/hu.js";
 import fi from "./locales/fi.js";
 import ua from "./locales/ua.js";
@@ -11,5 +12,4 @@ import fr from "./locales/fr.js";
 import ja from "./locales/ja.js";
 import pt from "./locales/pt.js";
 
-export { ar, az, es, en, fi, he, hu, pt, ja, fr, pl, ua };
-
+export { ar, az, es, en, fi, he, hu, pt, ja, fr, pl, ua, zhCN };
