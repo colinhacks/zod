5551dc511661e2c2fb661c85ee546ff3eb717813
Colin McDonnell <colinmcd94@gmail.com>
2025-05-15 21:13:13 -0700

    Update recursive type docs

--- play.ts
+++ play.ts
@@ -1,3 +1,25 @@
-import { z } from "zod/v4-mini";
+import { z } from "zod/v4";
 
-z;
+// z;
+
+// const A = z.object({
+//   type: z.literal("A"),
+//   get next(): z.ZodDiscriminatedUnion<[typeof A, typeof B]> {
+//     return z.discriminatedUnion([A, B]);
+//   },
+// });
+
+// const B = z.object({
+//   type: z.literal("B"),
+// });
+
+// const AB: z.ZodDiscriminatedUnion<[typeof A, typeof B]> = z.discriminatedUnion([A, B]);
+
+// import { z } from "zod/v4";
+
+const Activity = z.object({
+  name: z.string(),
+  get subactivities(): z.ZodUnion<[z.ZodNull, typeof Activity]> {
+    return z.union([z.null(), Activity]);
+  },
+});
