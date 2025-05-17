d52b531d9200eccf98ff82db20cb85109b6a5cdc
Colin McDonnell <colinmcd94@gmail.com>
2025-05-16 21:02:38 -0700

    Fix tests

--- play.ts
+++ play.ts
@@ -1,3 +1,12 @@
 import { z } from "zod/v4";
 
 z;
+
+const stringWithDefault = z
+  .pipe(
+    z.transform((v) => (v === "none" ? undefined : v)),
+    z.string()
+  )
+  .catch("default");
+
+stringWithDefault.parse(undefined);
