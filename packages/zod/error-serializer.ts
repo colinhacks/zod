e900dcec2d0f6db8f8762f18eb93219ce24882fa
Anna Bocharova <robin_tail@me.com>
2025-05-20 04:15:51 +0000

    fixed format.

--- packages/zod/error-serializer.ts
+++ packages/zod/error-serializer.ts
@@ -1,16 +1,10 @@
-import { SnapshotSerializer } from "vitest";
+import type { SnapshotSerializer } from "vitest";
 import { $ZodError } from "./src/v4/core/errors.js";
 
 export default {
   test: (subject) => subject instanceof $ZodError,
   serialize(error: $ZodError, config, indentation, depth, refs, printer) {
-    const {message, issues, name} = error;
-    return `${name} (${printer(
-      { message, issues },
-      config,
-      indentation,
-      depth,
-      refs,
-    )})`
+    const { message, issues, name } = error;
+    return `${name} (${printer({ message, issues }, config, indentation, depth, refs)})`;
   },
 } satisfies SnapshotSerializer as object;
--- packages/zod/vitest.config.ts
+++ packages/zod/vitest.config.ts
@@ -1,8 +1,11 @@
 import { defineProject, mergeConfig } from "vitest/config";
 import rootConfig from "../../vitest.root.mjs";
 
-export default mergeConfig(rootConfig, defineProject({
-  test: {
-    snapshotSerializers: ["./error-serializer.ts"]
-  }
-})) as object;
+export default mergeConfig(
+  rootConfig,
+  defineProject({
+    test: {
+      snapshotSerializers: ["./error-serializer.ts"],
+    },
+  })
+) as object;
