09e2aed5c07c77dc10f67034ae9c811580d55c8b
Anna Bocharova <robin_tail@me.com>
2025-05-20 06:35:38 +0000

    adjusting playground as suggested.

--- play.ts
+++ play.ts
@@ -1,5 +1,5 @@
 import z3 from "zod/v3";
 import z4 from "zod/v4";
 
-console.log("before (v3): ", z3.string().safeParse(123).error);
-console.log("after (v4):", z4.string().safeParse(123).error);
+console.log("before (v3): ", z3.string().safeParse(123).error?.message);
+console.log("after (v4):", z4.string().safeParse(123).error?.message);
