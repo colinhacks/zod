fe105a94364baa0dbb13ca83523c4514b75e7b58
Colin McDonnell <colinmcd94@gmail.com>
2025-05-20 17:13:36 -0700

    v3.25.9

--- play.ts
+++ play.ts
@@ -1,9 +1,3 @@
-import type { LanguageModelV1 } from "@ai-sdk/provider";
-import { generateObject } from "ai";
 import type { z } from "zod";
 
-export function myFn<T>(model: LanguageModelV1, schema: z.Schema<T>) {
-  return generateObject({ model, schema });
-}
-
-myFn;
+z;
