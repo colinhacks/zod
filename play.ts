e0fe7ae92876414946456890fed2f85802cadfd0
Colin McDonnell <colinmcd94@gmail.com>
2025-05-22 15:55:36 -0700

    Fix lint

--- play.ts
+++ play.ts
@@ -1,22 +1,3 @@
 import * as z from "zod/v4";
 
-z.custom<Date>((val) => val instanceof Date);
-
-z.string().min(5, {
-  error: (iss) => {
-    // customize "too_small" error message
-    if (iss.code === "too_small") {
-      return `Password must have ${iss.minimum} characters or more`;
-    }
-
-    // use default error otherwise
-    return undefined;
-  },
-});
-
-z.ZodError;
-
-const arg = z.custom<Date>((val) => val instanceof Date);
-z.date().check(z.custom((val) => val.getTime() > 0));
-
-// z.tuple<[title: string, text: string]>([z.string(), z.string()]);
+z;
