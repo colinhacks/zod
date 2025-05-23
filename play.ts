e012bd248d45d3204fb9554f99e8222426e29da6
Colin McDonnell <colinmcd94@gmail.com>
2025-05-23 13:32:50 -0700

    Improve override docs

--- play.ts
+++ play.ts
@@ -1,3 +1,20 @@
 import * as z from "zod/v4";
 
 z;
+
+const result = z.toJSONSchema(z.date(), {
+  unrepresentable: "any",
+  override: (ctx) => {
+    const def = ctx.zodSchema._zod.def;
+    if (def.type === "date") {
+      ctx.jsonSchema.type = "string";
+      ctx.jsonSchema.format = "date-time";
+    }
+  },
+});
+/* => {
+  type: 'string',
+  format: 'date-time',
+  '$schema': 'https://json-schema.org/draft/2020-12/schema'
+} */
+console.dir(result, { depth: null });
