7080d97997f91fcb7cde7ee91bd61563cd09c805
Colin McDonnell <colinmcd94@gmail.com>
2025-05-26 16:48:35 -0700

    Improve web url docs

--- packages/docs/pages/api/_og.tsx
+++ packages/docs/pages/api/_og.tsx
@@ -15,7 +15,7 @@ export const config = {
 export default async function (request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url);
-    const imagePrefix = process.env.VERCEL_URL ? `https://v4.zod.dev` : "http://localhost:3000";
+    const imagePrefix = process.env.VERCEL_URL ? `https://zod.dev` : "http://localhost:3000";
 
     // Get dynamic params
     const title = searchParams.get("title") || "Zod Documentation";
