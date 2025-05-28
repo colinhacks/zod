5bdeee4fee037f62f6b5f0d7f8b8f97ea2c17bed
Colin McDonnell <colinmcd94@gmail.com>
2025-05-28 15:23:29 -0700

    Abort further processing when transform is skipped (#4554)

--- play.ts
+++ play.ts
@@ -1,3 +1,21 @@
-import { z } from "zod/v4";
+import { z } from "zod/v3";
 
-z;
+// import * as z from "zod";
+
+const parseResult = z
+  .string()
+  .refine((x) => false) // force anything/everything to fail
+  .transform((x) => {
+    console.log("I don't get called"); // correct
+    return x.length;
+  })
+  .refine((x) => {
+    console.log("I shouldn't get called!!!!!!");
+    console.log(typeof x); // number
+    console.log(x);
+  })
+  .safeParse("123");
+
+console.log(`succeeded:  ${parseResult.success}`); // false (correct behavior)
+
+// z.string().min(1234, { abort: true });
