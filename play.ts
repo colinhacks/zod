289cba3b95e522b9c66611b94c218bcdfe8566b2
Raphaël Moreau <20722140+rphlmr@users.noreply.github.com>
2025-05-30 10:27:26 +0200

    Fix type inference in ZodMiniType check method (#4568)
    * Fix type inference in ZodMiniType check method and add tests for z.object().check()
    
    * Fix more references to _zod.output
    
    ---------
    
    Co-authored-by: Colin McDonnell <colinmcd94@gmail.com>

--- play.ts
+++ play.ts
@@ -1,10 +1,3 @@
 import { z } from "zod/v4-mini";
 
-const Thing = z
-  .object({
-    name: z.string(),
-    age: z.number(),
-  })
-  .check(({ value, issues }) => {
-    value; // => any
-  });
+z;
