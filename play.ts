50ef910565a14c127942442b7e09596afcfdca5f
Colin McDonnell <colinmcd94@gmail.com>
2025-06-03 17:49:05 -0700

    Add output type generic test

--- play.ts
+++ play.ts
@@ -1,3 +1,15 @@
 import { z } from "zod/v4";
 
 z;
+
+export const createV4Schema = <Output>(opts: {
+  schema: z.ZodType<Output>;
+}) => {
+  return opts.schema;
+};
+
+createV4Schema({
+  schema: z.object({
+    name: z.string(),
+  }),
+})._zod.output.name;
