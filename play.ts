716c319953c52ddd7958ae9a57c7d52e17475956
Colin McDonnell <colinmcd94@gmail.com>
2025-06-04 17:24:21 -0700

    Update hooks

--- play.ts
+++ play.ts
@@ -1,7 +1,5 @@
 import { z } from "zod/v4";
 
-// <T extends z.ZodType>(results: T) => z.strictObject({ results }).parse({}).results;
-
 const a = z.object({
   name: z.string().meta({ id: "first_name" }),
   email: z.string().email().meta({ id: "email_address" }),
