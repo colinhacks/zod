1aa5dbd24de3890df977b05a1fe29973bd0b5ec0
obha <obha121@outlook.com>
2025-06-07 14:44:45 +0100

    using simple functions instead of classes with only static members.

--- packages/zod/src/v3/tests/file.test.ts
+++ packages/zod/src/v3/tests/file.test.ts
@@ -3,45 +3,36 @@ import { expect, test } from "vitest";
 
 import * as z from "zod/v3";
 
-class MockFile {
-  static create(size?: number, name?: string, mimeType?: string) {
-    name = name || "mock.txt";
-    size = size || 1024;
-    mimeType = mimeType || "plain/txt";
+function file(size?: number, name?: string, mimeType?: string) {
+  name = name || "mock.txt";
+  size = size || 1024;
+  mimeType = mimeType || "plain/txt";
 
-    function range(count: number) {
-      let output = "";
-      for (let i = 0; i < count; i++) {
-        output += "a";
-      }
-      return output;
+  function range(count: number) {
+    let output = "";
+    for (let i = 0; i < count; i++) {
+      output += "a";
     }
-
-    return new File([range(size)], name, { type: mimeType });
+    return output;
   }
+
+  return new File([range(size)], name, { type: mimeType });
 }
 
 const basicSchema = z.file().size({ max: 50000, min: 1024 });
-const imageSchema = z
-  .file()
-  .size({ min: 20000 })
-  .mimeType("image/*,image/jpeg,image/png", "Invalide mime type");
+const imageSchema = z.file().size({ min: 20000 }).mimeType("image/*,image/jpeg,image/png", "Invalide mime type");
 
 test("passing validations", () => {
-  expect(basicSchema.parse(MockFile.create(1024))).toBeInstanceOf(File);
-  expect(basicSchema.parse(MockFile.create(50000))).toBeInstanceOf(File);
-  expect(basicSchema.parse(MockFile.create(2014))).toBeInstanceOf(File);
-  expect(
-    imageSchema.parse(MockFile.create(20000, "mock.jpeg", "image/jpeg")),
-  ).toBeInstanceOf(File);
+  expect(basicSchema.parse(file(1024))).toBeInstanceOf(File);
+  expect(basicSchema.parse(file(50000))).toBeInstanceOf(File);
+  expect(basicSchema.parse(file(2014))).toBeInstanceOf(File);
+  expect(imageSchema.parse(file(20000, "mock.jpeg", "image/jpeg"))).toBeInstanceOf(File);
 });
 
 test("failing validations", () => {
-  expect(() => basicSchema.parse(MockFile.create(1023))).toThrow();
-  expect(() => basicSchema.parse(MockFile.create(50001))).toThrow();
-  expect(() =>
-    imageSchema.parse(MockFile.create(20000, "mock.jpeg", "video/mp4")),
-  ).toThrow();
+  expect(() => basicSchema.parse(file(1023))).toThrow();
+  expect(() => basicSchema.parse(file(50001))).toThrow();
+  expect(() => imageSchema.parse(file(20000, "mock.jpeg", "video/mp4"))).toThrow();
   expect(() => basicSchema.parse(undefined)).toThrow();
   expect(() => basicSchema.parse({})).toThrow();
   expect(() => basicSchema.parse([])).toThrow();
