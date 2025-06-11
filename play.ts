afa7e672f4be0fc37ec9d35d281221ceb153baba
Colin McDonnell <colinmcd94@gmail.com>
2025-06-11 01:08:07 -0700

    3.25.61

--- play.ts
+++ play.ts
@@ -1,26 +1,10 @@
 import { z } from "zod/v4";
 
-const enumSchema = z.enum(["a", "b", "c"]);
-const nativeEnumSchema = z.nativeEnum(enumSchema.enum);
+const A = z.string().describe("name");
+const B = A.superRefine((val, ctx) => {
+  ctx.addIssue({
+    message: `Invalid ${A.description}`,
+  });
+});
 
-console.dir(nativeEnumSchema.safeParse("a"), { depth: null }); // Valid
-console.dir(nativeEnumSchema.safeParse("d"), { depth: null }); // Throws ZodError
-
-z.file().mime([""]);
-
-// function makeZodObj<const T extends string>(key: T) {
-//   const a: string = "a";
-//   const b: string = "b";
-//   return z.looseObject({
-//     [a]: z.string(),
-//     [b]: z.number(),
-//   });
-// }
-
-function makeZodObj<K extends string>(key: K) {
-  return z.record(z.literal(key), z.string());
-}
-
-const schema = makeZodObj("foo");
-type schema = z.infer<typeof schema>;
-// { foo: string; }
+B.parse("test"); // Should pass
