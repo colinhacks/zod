1614fd891b40cc9d23b249abad528e38ca718065
Colin McDonnell <colinmcd94@gmail.com>
2025-06-11 12:05:44 -0700

    3.25.62

--- play.ts
+++ play.ts
@@ -1,12 +1,3 @@
 import { z } from "zod/v4";
 
-const A = z.string().describe("name");
-const B = A.superRefine((val, ctx) => {
-  ctx.addIssue({
-    message: `Invalid ${A.description}`,
-  });
-});
-
-// B.parse("test"); // Should pass
-
-console.log(navigator, { depth: null });
+z;
