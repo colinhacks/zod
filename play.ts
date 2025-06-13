65309948ebcf926ee0570315b6254e06f59d01dd
Colin McDonnell <colinmcd94@gmail.com>
2025-06-13 03:10:46 -0400

    Clean up ecosystem

--- play.ts
+++ play.ts
@@ -13,3 +13,31 @@ const f = new File([""], "test.txt", {
   type: "text/plain",
 });
 file.parse(f);
+
+const Story = z.object({
+  type: z.literal("story"),
+  story: z.looseObject({}),
+});
+
+const Collection = z.object({
+  type: z.literal("collection"),
+  get items(): z.ZodArray<z.ZodDiscriminatedUnion<[typeof Story, typeof Collection]>> {
+    return z.array(z.discriminatedUnion("type", [Story, Collection]));
+  },
+});
+
+type Collection = z.infer<typeof Collection>;
+
+const ActivityUnion = z.union([
+  z.object({
+    name: z.string(),
+
+    // now it's `any` though the whole object is exactly the same
+    get subactivities(): z.ZodNullable<z.ZodArray<typeof ActivityUnion>> {
+      return z.nullable(z.array(ActivityUnion));
+    },
+  }),
+  z.string(),
+]);
+
+type A = z.infer<typeof ActivityUnion>;
