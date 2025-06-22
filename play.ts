4510bf3487e5634373a3ed934b71a077249b4736
Colin McDonnell <colinmcd94@gmail.com>
2025-06-21 21:02:47 -0700

    WIP

--- play.ts
+++ play.ts
@@ -1,12 +1,5 @@
 import { z } from "zod/v3";
-// import ru from "zod/v4/locales/ru.js";
-// console.dir(ru, { depth: null });
-// console.dir(z, { depth: null });
-// console.dir(
-//   z
-//     .optional(z.array(z.string()))
-//     .check(z.overwrite((data) => data ?? []))
-//     .parse(undefined),
-//   { depth: null }
-// );
-console.dir(z.string().parse("Hello, Zod!"), { depth: null });
+
+console.dir(z, { depth: null });
+const schema = z.string();
+console.dir(schema.parse("Hello, Zod!"), { depth: null });
