db354b6cd4e64a5e132d7b30a42394753b986e39
Colin McDonnell <colinmcd94@gmail.com>
2025-07-07 23:45:15 -0700

    Update docs

--- play.ts
+++ play.ts
@@ -1,65 +1,24 @@
 import { z } from "zod/v4";
 
-// const schema = z
-//   .object({
-//     password: z.string().min(8),
-//     confirmPassword: z.string(),
-//     anotherField: z.string(),
-//   })
-//   .refine((data) => data.password === data.confirmPassword, {
-//     message: "Passwords do not match",
-//     path: ["confirmPassword"],
-//     when(payload) {
-//       if (payload.value === undefined) return false;
-//       if (payload.value === null) return false;
-//       // no issues with confirmPassword or password
-//       return payload.issues.every((iss) => iss.path?.[0] !== "confirmPassword" && iss.path?.[0] !== "password");
-//     },
-//   });
-
 const schema = z
   .object({
     password: z.string().min(8),
     confirmPassword: z.string(),
-    anotherField: z.string(),
   })
-  .check(
-    // z.refine((data) => data.password === data.confirmPassword, {
-    //   message: "Passwords do not match",
-    //   path: ["confirmPassword"],
-    //   when(payload) {
-    //     if (payload.value === undefined) return false;
-    //     if (payload.value === null) return false;
-    //     // no issues with confirmPassword or password
-    //     return payload.issues.every((iss) => iss.path?.[0] !== "confirmPassword" && iss.path?.[0] !== "password");
-    //   },
-    // })
-    z.check((ctx) => {
-      if (ctx.value.password === ctx.value.confirmPassword) {
-        ctx.issues.push({
-          code: "custom",
-          message: "Passwords do not match",
-          input: ctx.value,
-        });
-      }
-    })
+  .refine(
+    (data) => {
+      console.log("pass check", data);
+      return data.password === data.confirmPassword;
+    },
+    {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    }
   );
 
-schema.parse({
-  password: "asdf",
-  confirmPassword: "asdf",
-  anotherField: 1234, // ❌ error
-});
-
-z.string().check((ctx) => {
-  if (ctx.value === "asdf") {
-    ctx.issues.push({
-      code: "custom",
-      message: "No asdf allowed",
-      input: ctx.value,
-    });
-  }
-});
-schema.parse(null);
-
-z.string().refine;
+console.log(
+  schema.safeParse({
+    password: "asdf",
+    confirmPassword: "asdff",
+  })
+);
