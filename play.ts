0b65a73d6d0b94e8d54cc410dc972144828e5ee4
Colin McDonnell <colinmcd94@gmail.com>
2025-07-08 21:13:29 -0700

    Update play

--- play.ts
+++ play.ts
@@ -1,5 +1,5 @@
-import * as z from "zod";           // Zod 3
-import * as z from "zod/v4";        // Zod 4
-import * as z from "zod/v4-mini";   // Zod 4 Mini
+import * as z from "zod";           // Zod 4
+import * as z from "zod/mini";      // Zod 4 Mini
+import * as z from "zod/v3";        // Zod 3
 
 z;
