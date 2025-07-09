7c4c83c5f421e3d4d65fed79491628723821eee4
Colin McDonnell <colinmcd94@gmail.com>
2025-07-09 16:09:24 -0700

    Update mime docs

--- play.ts
+++ play.ts
@@ -1,3 +1,28 @@
 import * as z from "zod";
 
 z;
+
+const schema = z
+  .object({
+    password: z.string(),
+    confirm: z.string(),
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: "Passwords don't match",
+    path: ["confirm"],
+  });
+
+console.log(
+  schema.safeParse({
+    password: "asdf",
+    confirm: "asdF",
+  }).success
+);
+
+console.log(
+  schema.safeParse({
+    password: "asdf",
+    confirm: "asdf",
+  }).success
+);
+z.file().mime("image/png");
