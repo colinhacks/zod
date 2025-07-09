65b14ede4b023024bed27c121e1673ddccd6437d
Colin McDonnell <colinmcd94@gmail.com>
2025-07-09 11:44:27 -0700

    Fix lint

--- play.ts
+++ play.ts
@@ -1,8 +1,3 @@
-// after upgrading to zod@4.0.0:
-import * as z from "zod"; // Zod 4 (regular)
-// import * as z from "zod/mini" // Zod 4 Mini
-// import * as z from "zod/v3";  // Zod 3
+import * as z from "zod";
 
-// // these still work, but are no longer needed
-// import * as z from "zod/v4";
-// import * as z from "zod/v4-mini";
+z;
