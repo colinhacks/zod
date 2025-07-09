01f80b31578669d76cb9084b6c55760c8f0dbe2d
Colin McDonnell <colinmcd94@gmail.com>
2025-07-09 11:43:05 -0700

    4.0.0

--- play.ts
+++ play.ts
@@ -1,3 +1,8 @@
-import * as z from "zod"; // Zod 4
+// after upgrading to zod@4.0.0:
+import * as z from "zod"; // Zod 4 (regular)
+// import * as z from "zod/mini" // Zod 4 Mini
+// import * as z from "zod/v3";  // Zod 3
 
-z;
+// // these still work, but are no longer needed
+// import * as z from "zod/v4";
+// import * as z from "zod/v4-mini";
