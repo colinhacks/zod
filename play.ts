f74923d46e04b82d665f4bf55008c9bc8fe7f79b
Colin McDonnell <colinmcd94@gmail.com>
2025-07-08 22:18:22 -0700

    Add versioning.mdx

--- play.ts
+++ play.ts
@@ -1,5 +1,3 @@
-import * as z from "zod";           // Zod 4
-import * as z from "zod/mini";      // Zod 4 Mini
-import * as z from "zod/v3";        // Zod 3
+import * as z from "zod"; // Zod 4
 
 z;
