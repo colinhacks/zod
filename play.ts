45afab0f846dffd591362b6f770017507eb185b5
Colin McDonnell <colinmcd94@gmail.com>
2025-07-10 15:34:43 -0700

    4.0.5

--- play.ts
+++ play.ts
@@ -1,8 +1,3 @@
 import { z } from "zod";
 
-const Parent = z.object({ key1: z.object({ key2: z.string() }) });
-
-console.log(Parent._zod.def.shape.key1._zod.run);
-// => [Function (anonymous)]
-
-console.log(z.parse(Parent._zod.def.shape.key1, { key2: "asdf" }));
+z;
