d8a564bc7f409b30c284f21c6f4fcedc59203fb0
Colin McDonnell <colinmcd94@gmail.com>
2025-07-22 01:07:52 -0700

    Escape non-string literal patterns. Closes #4894

--- play.ts
+++ play.ts
@@ -1,12 +1,7 @@
 import * as z from "zod";
 
-const customRegistry = z.registry();
-//
-const schema1 = z.string();
-console.log(customRegistry.get(schema1)); // undefined
-//
-const schema2 = z.string().meta({ title: "Schema" });
-console.log(customRegistry.get(schema2)); // {}
-//
-const schema3 = z.string().register(z.globalRegistry, { title: "Schema" });
-console.log(customRegistry.get(schema3)); // undefined
+z.number({
+  error: "positiveNumber",
+})
+  .int("positiveNumber")
+  .min(1, "positiveNumber");
