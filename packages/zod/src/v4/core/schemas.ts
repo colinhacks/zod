f75d85294df2ff4ccea056e8eeb7c14750fa929f
andrew jarrett <ahrjarrett@gmail.com>
2025-07-23 17:28:17 -0500

    fix: escapes decimal place in `z.literal` (#4895)
    * fix: escapes decimal place in `z.literal`
    
    * fix(core): aligns implementation with v4.0.6
    
    * adds literal tests

--- packages/zod/src/v4/core/schemas.ts
+++ packages/zod/src/v4/core/schemas.ts
@@ -2795,7 +2795,7 @@ export const $ZodLiteral: core.$constructor<$ZodLiteral> = /*@__PURE__*/ core.$c
     inst._zod.pattern = new RegExp(
       `^(${def.values
 
-        .map((o) => (typeof o === "string" ? util.escapeRegex(o) : o ? o.toString() : String(o)))
+        .map((o) => (typeof o === "string" ? util.escapeRegex(o) : o ? util.escapeRegex(o.toString()) : String(o)))
         .join("|")})$`
     );
 
