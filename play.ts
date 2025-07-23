06e9782dbc005a53b349fe1da7ae2cb4c2e27d1c
Colin McDonnell <colinmcd94@gmail.com>
2025-07-23 14:36:12 -0700

    Tweak URL docs. Closes #4959

--- play.ts
+++ play.ts
@@ -1,30 +1,3 @@
 import * as z from "zod";
 
-const NodeBase = z.object({
-  id: z.string(),
-  get children() {
-    console.log("accessed");
-    return z.array(Node).optional();
-  },
-});
-
-// typed as any
-console.log("nodeone");
-const NodeOne = NodeBase.extend({
-  name: z.literal("nodeOne"),
-  // if this is commented out, NodeTwo becomes any
-  get children() {
-    return z.array(Node);
-  },
-});
-
-const NodeTwo = NodeBase.extend({
-  name: z.literal("nodeTwo"),
-  get children() {
-    return z.array(Node);
-  },
-});
-
-const Node = z.union([NodeOne, NodeTwo]);
-
-z.object;
+console.log(z.url().parse("sup")); // ❌
