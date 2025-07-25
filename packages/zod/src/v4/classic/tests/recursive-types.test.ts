9e4dfa03504b367d170b76451533fee5b96492c1
Colin McDonnell <colinmcd94@gmail.com>
2025-07-24 23:31:47 -0700

    Improve recursion for intersections. Closes #4714

--- packages/zod/src/v4/classic/tests/recursive-types.test.ts
+++ packages/zod/src/v4/classic/tests/recursive-types.test.ts
@@ -260,6 +260,45 @@ test("mutual recursion with meta", () => {
   expectTypeOf<B>().toEqualTypeOf<_B>();
 });
 
+test("intersection with recursive types", () => {
+  const A = z.discriminatedUnion("type", [
+    z.object({
+      type: z.literal("CONTAINER"),
+    }),
+    z.object({
+      type: z.literal("SCREEN"),
+      config: z.object({ x: z.number(), y: z.number() }),
+    }),
+  ]);
+  type A = z.infer<typeof A>;
+
+  const B = z.object({
+    get children() {
+      return z.array(C).optional();
+    },
+  });
+  type B = z.infer<typeof B>;
+
+  const C = z.intersection(A, B);
+  type C = z.infer<typeof C>;
+
+  type _C = (
+    | {
+        type: "CONTAINER";
+      }
+    | {
+        type: "SCREEN";
+        config: {
+          x: number;
+          y: number;
+        };
+      }
+  ) & {
+    children?: _C[] | undefined;
+  };
+  expectTypeOf<C>().toEqualTypeOf<_C>();
+});
+
 test("object utilities with recursive types", () => {
   const NodeBase = z.object({
     id: z.string(),
--- play.ts
+++ play.ts
@@ -1,3 +1,26 @@
 import * as z from "zod";
 
-z;
+// import { z } from "zod/v4";
+
+export const A = z.discriminatedUnion("type", [
+  z.object({
+    type: z.literal("CONTAINER"),
+  }),
+  z.object({
+    type: z.literal("SCREEN"),
+    config: z.object({ x: z.number(), y: z.number() }),
+  }),
+]);
+type A = z.infer<typeof A>;
+
+const B = z.object({
+  get children() {
+    return z.array(C).optional();
+  },
+});
+type B = z.infer<typeof B>;
+
+const C = z.intersection(A, B);
+type C = z.infer<typeof C>;
+
+export type ElementInput = z.infer<typeof C>;
