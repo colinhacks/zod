5ec5bef5bc494928c61d5d61af62503cabd7358b
Colin McDonnell <colinmcd94@gmail.com>
2025-07-24 18:09:21 -0700

    WIP

--- play.ts
+++ play.ts
@@ -1,9 +1,19 @@
 import * as z from "zod";
 
-const SomeSchema = z.discriminatedUnion("type", [
-  z.object({ type: z.literal("foo"), foo: z.string() }),
-  z.object({ type: z.literal("bar"), bar: z.string() }),
-  z.object({ type: z.literal("baz"), baz: z.string() }),
-]);
+// I'd recommend using a union of literals in conjunction with Zod 4's metadata API: https://zod.dev/metadata
 
-SomeSchema.parse({ type: "qux" });
+const foo = z.literal("foo").describe("The foo value");
+const bar = z.literal("bar").describe("The bar value");
+const baz = z.literal("baz").describe("The baz value");
+
+const schema = z.union([foo, bar, baz]);
+
+console.log(z.toJSONSchema(schema));
+// {
+//   '$schema': 'https://json-schema.org/draft/2020-12/schema',
+//   anyOf: [
+//     { description: 'The foo value', type: 'string', const: 'foo' },
+//     { description: 'The bar value', type: 'string', const: 'bar' },
+//     { description: 'The baz value', type: 'string', const: 'baz' }
+//   ]
+// }
