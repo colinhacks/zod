c5916ba6eb733b18f86e3d04d2fe6648bd33f717
Colin McDonnell <colinmcd94@gmail.com>
2025-07-25 00:10:59 -0700

    Improve offset regex. Closes #3883

--- play.ts
+++ play.ts
@@ -1,26 +1,6 @@
 import * as z from "zod";
 
-// import { z } from "zod/v4";
-
-export const A = z.discriminatedUnion("type", [
-  z.object({
-    type: z.literal("CONTAINER"),
-  }),
-  z.object({
-    type: z.literal("SCREEN"),
-    config: z.object({ x: z.number(), y: z.number() }),
-  }),
-]);
-type A = z.infer<typeof A>;
-
-const B = z.object({
-  get children() {
-    return z.array(C).optional();
-  },
-});
-type B = z.infer<typeof B>;
-
-const C = z.intersection(A, B);
-type C = z.infer<typeof C>;
-
-export type ElementInput = z.infer<typeof C>;
+// console.log(z.string().datetime().safeParse("2022-01-01T00:99:00Z")); // fails
+//                                         ^
+console.log(z.string().datetime({ offset: true }).safeParse("2022-01-01T00:00:00+99:00")); // passes
+//                                               ^
