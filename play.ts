ff89187172ac6aa4a6264592980d1628ee785242
Colin McDonnell <colinmcd94@gmail.com>
2025-08-20 23:58:46 -0700

    Resizing

--- play.ts
+++ play.ts
@@ -1,13 +1,3 @@
 import { z } from "zod/v4";
 
 z;
-
-// import { z } from "zod";
-const BrandedSchema = z.string().brand<"brand">();
-const WrappedSchema = z.object({ key: BrandedSchema });
-
-type bso = typeof BrandedSchema._output;
-type basdfasdfso = typeof BrandedSchema._zod.output;
-type wso = (typeof WrappedSchema._output)["key"];
-type bso2 = z.output<typeof BrandedSchema>;
-type wso2 = z.output<typeof WrappedSchema>["key"];
