f3f095560c395c8e4209fc2bfce8f5c6f298de52
Colin McDonnell <colinmcd94@gmail.com>
2025-08-20 18:32:47 -0700

    Update disc union docs. Closes #5108

--- play.ts
+++ play.ts
@@ -1,23 +1,15 @@
 import * as z from "zod/v4";
 
-const jsonCodec = z.codec(z.string(), z.json(), {
-  decode: (jsonString, ctx) => {
-    try {
-      return JSON.parse(jsonString);
-    } catch (_) {
-      ctx.issues.push({
-        code: "invalid_format",
-        format: "json",
-        input: jsonString,
-      });
-      return z.NEVER;
-    }
-  },
-  encode: (value) => JSON.stringify(value),
-});
+const A = z.discriminatedUnion("type", [
+  z.object({
+    type: z.number(),
+    a: z.string(),
+  }),
+  z.object({
+    type: z.string(),
+    b: z.string(),
+  }),
+]);
 
-// to further validate the resulting JSON data
-// pipe the result into another schema
-const myCodec = jsonCodec.pipe(z.object({ name: z.string(), age: z.number() }));
-z.decode(myCodec, '{"name":,"age":30}'); // => { name: "Alice", age: 30 }
-z.encode(myCodec, { name: "Bob", age: 25 }); // => '{"name":"Bob","age":25}'
+A.parse({ type: 1, a: "a" });
+A.parse({ type: "b", b: "b" });
