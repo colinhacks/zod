7e39a99a88a9b7f0c9c3d5fd40fbbab494365c9a
Colin McDonnell <colinmcd94@gmail.com>
2025-08-23 11:38:23 -0700

    Improve codec docs

--- play.ts
+++ play.ts
@@ -2,13 +2,17 @@ import * as z from "zod";
 
 z;
 
-const stringToDate = z.codec(
-  z.iso.datetime(), // input schema: ISO date string
-  z.date(), // output schema: Date object
-  {
-    decode: (isoString) => new Date(isoString),
-    encode: (date) => date.toISOString(),
-  }
-);
+export const Field1Schema = z.strictObject({
+  "field1.1": z.string(),
+  "field1.2": z.number(),
+});
 
-const schema = stringToDate.refine((date) => date.getFullYear() > 2000, "must be after 2000");
+export const Field2Schema = z.strictObject({
+  "field2.1": z.boolean(),
+  "field2.2": z.date(),
+});
+
+export const SomeSchema = z.strictObject({
+  field1: Field1Schema as typeof Field1Schema,
+  field2: Field2Schema as typeof Field2Schema,
+});
