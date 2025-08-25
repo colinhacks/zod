940383d0523da41c4e2422b76455da39dddd6c4f
Colin McDonnell <colinmcd94@gmail.com>
2025-08-25 16:02:48 -0700

    Update JSON codec and docs

--- play.ts
+++ play.ts
@@ -1,3 +1,22 @@
 import * as z from "zod";
 
 z;
+const json = <T extends z.core.$ZodType>(schema: T) =>
+  z.codec(z.string(), schema, {
+    decode: (jsonString, ctx) => {
+      try {
+        return JSON.parse(jsonString);
+      } catch (err: any) {
+        ctx.issues.push({
+          code: "invalid_format",
+          format: "json",
+          input: jsonString,
+          message: err.message,
+        });
+        return z.NEVER;
+      }
+    },
+    encode: (value) => JSON.stringify(value),
+  });
+
+console.log(json(z.object({ name: z.string() })).parse(`{"name":"colin"}`));
