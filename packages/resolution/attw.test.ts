41eb0ebe84a7d5641182a05852c1070cbfc49f28
Mark Vayngrib <mvayngrib@gmail.com>
2025-09-25 13:11:28 -0400

    chore: update snapshot

--- packages/resolution/attw.test.ts
+++ packages/resolution/attw.test.ts
@@ -25,6 +25,47 @@ describe("Are The Types Wrong (attw) tests", () => {
     const output = result.stdout + (result.stderr ? "\n" + result.stderr : "");
     // remove first line
     const outputWithoutFirstLine = output.split("\n").slice(2).join("\n").trim();
-    expect(outputWithoutFirstLine).toMatchInlineSnapshot();
+    expect(outputWithoutFirstLine).toMatchInlineSnapshot(`
+      "💀 Import failed to resolve to type declarations or JavaScript files. https://github.com/arethetypeswrong/arethetypeswrong.github.io/blob/main/docs/problems/NoResolution.md
+
+      🐛 Import resolved to types through a conditional package.json export, but only after failing to resolve through an earlier condition. This behavior is a TypeScript bug (https://github.com/microsoft/TypeScript/issues/50762). It may misrepresent the runtime behavior of this import and should not be relied upon. https://github.com/arethetypeswrong/arethetypeswrong.github.io/blob/main/docs/problems/FallbackCondition.md
+
+
+      "@exodus/zod/package.json"
+
+      node10: 🟢 (JSON)
+      node16 (from CJS): 🟢 (JSON)
+      node16 (from ESM): 🟢 (JSON)
+      bundler: 🟢 (JSON)
+
+      ***********************************
+
+      "@exodus/zod"
+
+      node10: 🟢 
+      node16 (from CJS): 💀 Resolution failed
+      node16 (from ESM): 🐛 Used fallback condition
+      bundler: 🐛 Used fallback condition
+
+      ***********************************
+
+      "@exodus/zod/v4"
+
+      node10: 🟢 
+      node16 (from CJS): 💀 Resolution failed
+      node16 (from ESM): 🐛 Used fallback condition
+      bundler: 🐛 Used fallback condition
+
+      ***********************************
+
+      "@exodus/zod/v4/core"
+
+      node10: 🟢 
+      node16 (from CJS): 💀 Resolution failed
+      node16 (from ESM): 🐛 Used fallback condition
+      bundler: 🐛 Used fallback condition
+
+      ***********************************"
+    `);
   }, 30000); // 30 second timeout for the command
 });
