b5c44a1b7289998b6a38c3c30650b12a582d8d9f
Colin McDonnell <colinmcd94@gmail.com>
2025-10-15 10:21:00 -0700

    Clean up

--- packages/zod/src/index.ts
+++ packages/zod/src/index.ts
@@ -1,9 +1,3 @@
-import z4 from "./v4/classic/index.js";
+import * as z4 from "./v4/classic/index.js";
 export * from "./v4/classic/index.js";
-// export { z };
 export default z4;
-
-// import z4 from "./classic/index.js";
-// export * from "./classic/index.js";
-
-// export default z4;
