a4e4bc80e204577c698cf1369dd63c2b986d35f3
Colin McDonnell <colinmcd94@gmail.com>
2025-10-21 15:16:34 -0700

    Lock to node 24

--- play.ts
+++ play.ts
@@ -2,3 +2,12 @@ import * as z from "zod";
 
 const a = z.uint32();
 console.log(a.parse(1234));
+
+const arg = z
+  .object({
+    a: z.string(),
+    b: z.string(),
+  })
+  .pick({} as Record<string, true>);
+
+arg.parse({});
