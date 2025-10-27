864d6184d142bb1d641bb4d555aa28a4f38da0c5
Szymon Gebler <gebler.szymon@gmail.com>
2025-10-27 18:31:34 +0100

    Convert dates to numbers in `too_big` and `too_small` issues
    This change aligns the runtime values with typings. Detection of date
    objects is implemented by checking if `typeof` returns `object`, as the
    same check is used to initialize `origin`.
    
    Furthermore, this changes initialization of test values so that they do
    not rely on local time zone. (`Date` constructor with multiple numeric
    parameters interprets them in the local time zone.)

--- packages/zod/src/v4/core/errors.ts
+++ packages/zod/src/v4/core/errors.ts
@@ -26,7 +26,7 @@ export interface $ZodIssueInvalidType<Input = unknown> extends $ZodIssueBase {
 export interface $ZodIssueTooBig<Input = unknown> extends $ZodIssueBase {
   readonly code: "too_big";
   readonly origin: "number" | "int" | "bigint" | "date" | "string" | "array" | "set" | "file" | (string & {});
-  readonly maximum: number | bigint | Date;
+  readonly maximum: number | bigint;
   readonly inclusive?: boolean;
   readonly exact?: boolean;
   readonly input?: Input;
@@ -35,7 +35,7 @@ export interface $ZodIssueTooBig<Input = unknown> extends $ZodIssueBase {
 export interface $ZodIssueTooSmall<Input = unknown> extends $ZodIssueBase {
   readonly code: "too_small";
   readonly origin: "number" | "int" | "bigint" | "date" | "string" | "array" | "set" | "file" | (string & {});
-  readonly minimum: number | bigint | Date;
+  readonly minimum: number | bigint;
   /** True if the allowable range includes the minimum */
   readonly inclusive?: boolean;
   /** True if the allowed value is fixed (e.g.` z.length(5)`), not a range (`z.minLength(5)`) */
