3e8ba43d21a4a5fd0f55ca86d2e9b0a892d17c3d
Colin McDonnell <colinmcd94@gmail.com>
2025-11-24 23:07:08 -0800

    Add back files

---
+++ packages/resolution/vitest.config.js
@@ -0,0 +1,11 @@
+import { defineConfig, mergeConfig } from "vitest/config";
+import rootConfig from "../../vitest.root.mjs";
+export default mergeConfig(rootConfig, defineConfig({
+    test: {
+        globals: true,
+        environment: "node",
+        typecheck: {
+            enabled: false,
+        },
+    },
+}));
