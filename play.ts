947b4eb281cd4d860feddd6618934faceb38c2d6
Colin McDonnell <colinmcd94@gmail.com>
2025-12-22 12:45:03 -0800

    Document z.meta and z.describe

--- play.ts
+++ play.ts
@@ -3,3 +3,24 @@ import * as z from "zod";
 z.string().brand<"A", "in">();
 z.string().brand<"A", "out">();
 z.string().brand<"A", "inout">();
+
+const baseSchema = z.object({
+  value: z.number(),
+  allowsNegative: z.boolean(),
+  somethingElse: z.string(),
+});
+
+const refinement = z.refine<{ value: number; allowsNegative: boolean }>(({ value, allowsNegative }) => {
+  if (allowsNegative) {
+    return true;
+  }
+  return value >= 0;
+});
+
+const schema = baseSchema.check(refinement);
+
+const schemaB = baseSchema
+  .safeOmit({
+    somethingElse: true,
+  })
+  .check(refinement);
