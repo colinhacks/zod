613e295b52c0aaf54c60e4d0b2a2d6cc864f34c6
Prakhar Chhalotre <chhalotreprakhar00@gmail.com>
2026-01-03 22:31:33 +0530

    feat: add iso yearMonth, monthDay, instant and zonedDateTime as per Temporal

--- packages/zod/src/v4/classic/iso.ts
+++ packages/zod/src/v4/classic/iso.ts
@@ -88,3 +88,95 @@ export const ZodISODuration: core.$constructor<ZodISODuration> = /*@__PURE__*/ c
 export function duration(params?: string | core.$ZodISODurationParams): ZodISODuration {
   return core._isoDuration(ZodISODuration, params);
 }
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////                          //////////
+//////////    ZodISOYearMonth       //////////
+//////////                          //////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+export interface ZodISOYearMonth extends schemas.ZodStringFormat {
+  _zod: core.$ZodISOYearMonthInternals;
+}
+export const ZodISOYearMonth: core.$constructor<ZodISOYearMonth> = /*@__PURE__*/ core.$constructor(
+  "ZodISOYearMonth",
+  (inst, def) => {
+    core.$ZodISOYearMonth.init(inst, def);
+    schemas.ZodStringFormat.init(inst, def);
+  }
+);
+
+export function yearMonth(params?: string | core.$ZodISOYearMonthParams): ZodISOYearMonth {
+  return core._isoYearMonth(ZodISOYearMonth, params);
+}
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////                          //////////
+//////////     ZodISOMonthDay       //////////
+//////////                          //////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+export interface ZodISOMonthDay extends schemas.ZodStringFormat {
+  _zod: core.$ZodISOMonthDayInternals;
+}
+export const ZodISOMonthDay: core.$constructor<ZodISOMonthDay> = /*@__PURE__*/ core.$constructor(
+  "ZodISOMonthDay",
+  (inst, def) => {
+    core.$ZodISOMonthDay.init(inst, def);
+    schemas.ZodStringFormat.init(inst, def);
+  }
+);
+
+export function monthDay(params?: string | core.$ZodISOMonthDayParams): ZodISOMonthDay {
+  return core._isoMonthDay(ZodISOMonthDay, params);
+}
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////                          //////////
+//////////      ZodISOInstant       //////////
+//////////                          //////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+export interface ZodISOInstant extends schemas.ZodStringFormat {
+  _zod: core.$ZodISOInstantInternals;
+}
+export const ZodISOInstant: core.$constructor<ZodISOInstant> = /*@__PURE__*/ core.$constructor(
+  "ZodISOInstant",
+  (inst, def) => {
+    core.$ZodISOInstant.init(inst, def);
+    schemas.ZodStringFormat.init(inst, def);
+  }
+);
+
+export function instant(params?: string | core.$ZodISOInstantParams): ZodISOInstant {
+  return core._isoInstant(ZodISOInstant, params);
+}
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////                              //////////
+//////////    ZodISOZonedDateTime       //////////
+//////////                              //////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+export interface ZodISOZonedDateTime extends schemas.ZodStringFormat {
+  _zod: core.$ZodISOZonedDateTimeInternals;
+}
+export const ZodISOZonedDateTime: core.$constructor<ZodISOZonedDateTime> = /*@__PURE__*/ core.$constructor(
+  "ZodISOZonedDateTime",
+  (inst, def) => {
+    core.$ZodISOZonedDateTime.init(inst, def);
+    schemas.ZodStringFormat.init(inst, def);
+  }
+);
+
+export function zonedDateTime(params?: string | core.$ZodISOZonedDateTimeParams): ZodISOZonedDateTime {
+  return core._isoZonedDateTime(ZodISOZonedDateTime, params);
+}
