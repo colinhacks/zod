d55122eb6cf04e2bf99328ddd3685d75501b50c8
Colin McDonnell <colinmcd94@gmail.com>
2026-04-30 09:56:47 -0700

    fix(v4): preserve callsites in parse stack traces

--- packages/zod/src/v4/core/parse.ts
+++ packages/zod/src/v4/core/parse.ts
@@ -4,27 +4,35 @@ import type * as schemas from "./schemas.js";
 import * as util from "./util.js";
 
 export type $ZodErrorClass = { new (issues: errors.$ZodIssue[]): errors.$ZodError };
+type $ParseParams = { callee?: util.AnyFunc; Err?: $ZodErrorClass };
+
+function finalizeParams(callee: util.AnyFunc, params: $ParseParams | undefined): $ParseParams {
+  return params?.Err ? { callee: params.callee ?? callee, Err: params.Err } : { callee: params?.callee ?? callee };
+}
 
 ///////////        METHODS       ///////////
 export type $Parse = <T extends schemas.$ZodType>(
   schema: T,
   value: unknown,
   _ctx?: schemas.ParseContext<errors.$ZodIssue>,
-  _params?: { callee?: util.AnyFunc; Err?: $ZodErrorClass }
+  _params?: $ParseParams
 ) => core.output<T>;
 
-export const _parse: (_Err: $ZodErrorClass) => $Parse = (_Err) => (schema, value, _ctx, _params) => {
-  const ctx: schemas.ParseContextInternal = _ctx ? { ..._ctx, async: false } : { async: false };
-  const result = schema._zod.run({ value, issues: [] }, ctx);
-  if (result instanceof Promise) {
-    throw new core.$ZodAsyncError();
-  }
-  if (result.issues.length) {
-    const e = new (_params?.Err ?? _Err)(result.issues.map((iss) => util.finalizeIssue(iss, ctx, core.config())));
-    util.captureStackTrace(e, _params?.callee);
-    throw e;
-  }
-  return result.value as core.output<typeof schema>;
+export const _parse: (_Err: $ZodErrorClass) => $Parse = (_Err) => {
+  const fn: $Parse = (schema, value, _ctx, _params) => {
+    const ctx: schemas.ParseContextInternal = _ctx ? { ..._ctx, async: false } : { async: false };
+    const result = schema._zod.run({ value, issues: [] }, ctx);
+    if (result instanceof Promise) {
+      throw new core.$ZodAsyncError();
+    }
+    if (result.issues.length) {
+      const e = new (_params?.Err ?? _Err)(result.issues.map((iss) => util.finalizeIssue(iss, ctx, core.config())));
+      util.captureStackTrace(e, _params?.callee ?? fn);
+      throw e;
+    }
+    return result.value as core.output<typeof schema>;
+  };
+  return fn;
 };
 
 export const parse: $Parse = /* @__PURE__*/ _parse(errors.$ZodRealError);
@@ -33,19 +41,22 @@ export type $ParseAsync = <T extends schemas.$ZodType>(
   schema: T,
   value: unknown,
   _ctx?: schemas.ParseContext<errors.$ZodIssue>,
-  _params?: { callee?: util.AnyFunc; Err?: $ZodErrorClass }
+  _params?: $ParseParams
 ) => Promise<core.output<T>>;
 
-export const _parseAsync: (_Err: $ZodErrorClass) => $ParseAsync = (_Err) => async (schema, value, _ctx, params) => {
-  const ctx: schemas.ParseContextInternal = _ctx ? { ..._ctx, async: true } : { async: true };
-  let result = schema._zod.run({ value, issues: [] }, ctx);
-  if (result instanceof Promise) result = await result;
-  if (result.issues.length) {
-    const e = new (params?.Err ?? _Err)(result.issues.map((iss) => util.finalizeIssue(iss, ctx, core.config())));
-    util.captureStackTrace(e, params?.callee);
-    throw e;
-  }
-  return result.value as core.output<typeof schema>;
+export const _parseAsync: (_Err: $ZodErrorClass) => $ParseAsync = (_Err) => {
+  const fn: $ParseAsync = async (schema, value, _ctx, params) => {
+    const ctx: schemas.ParseContextInternal = _ctx ? { ..._ctx, async: true } : { async: true };
+    let result = schema._zod.run({ value, issues: [] }, ctx);
+    if (result instanceof Promise) result = await result;
+    if (result.issues.length) {
+      const e = new (params?.Err ?? _Err)(result.issues.map((iss) => util.finalizeIssue(iss, ctx, core.config())));
+      util.captureStackTrace(e, params?.callee ?? fn);
+      throw e;
+    }
+    return result.value as core.output<typeof schema>;
+  };
+  return fn;
 };
 
 export const parseAsync: $ParseAsync = /* @__PURE__*/ _parseAsync(errors.$ZodRealError);
@@ -97,12 +108,17 @@ export const safeParseAsync: $SafeParseAsync = /* @__PURE__*/ _safeParseAsync(er
 export type $Encode = <T extends schemas.$ZodType>(
   schema: T,
   value: core.output<T>,
-  _ctx?: schemas.ParseContext<errors.$ZodIssue>
+  _ctx?: schemas.ParseContext<errors.$ZodIssue>,
+  _params?: $ParseParams
 ) => core.input<T>;
 
-export const _encode: (_Err: $ZodErrorClass) => $Encode = (_Err) => (schema, value, _ctx) => {
-  const ctx = _ctx ? { ..._ctx, direction: "backward" as const } : { direction: "backward" as const };
-  return _parse(_Err)(schema, value, ctx as any) as any;
+export const _encode: (_Err: $ZodErrorClass) => $Encode = (_Err) => {
+  const parse = _parse(_Err);
+  const fn: $Encode = (schema, value, _ctx, _params) => {
+    const ctx = _ctx ? { ..._ctx, direction: "backward" as const } : { direction: "backward" as const };
+    return parse(schema, value, ctx as any, finalizeParams(fn, _params)) as any;
+  };
+  return fn;
 };
 
 export const encode: $Encode = /* @__PURE__*/ _encode(errors.$ZodRealError);
@@ -110,11 +126,16 @@ export const encode: $Encode = /* @__PURE__*/ _encode(errors.$ZodRealError);
 export type $Decode = <T extends schemas.$ZodType>(
   schema: T,
   value: core.input<T>,
-  _ctx?: schemas.ParseContext<errors.$ZodIssue>
+  _ctx?: schemas.ParseContext<errors.$ZodIssue>,
+  _params?: $ParseParams
 ) => core.output<T>;
 
-export const _decode: (_Err: $ZodErrorClass) => $Decode = (_Err) => (schema, value, _ctx) => {
-  return _parse(_Err)(schema, value, _ctx);
+export const _decode: (_Err: $ZodErrorClass) => $Decode = (_Err) => {
+  const parse = _parse(_Err);
+  const fn: $Decode = (schema, value, _ctx, _params) => {
+    return parse(schema, value, _ctx, finalizeParams(fn, _params));
+  };
+  return fn;
 };
 
 export const decode: $Decode = /* @__PURE__*/ _decode(errors.$ZodRealError);
@@ -122,12 +143,17 @@ export const decode: $Decode = /* @__PURE__*/ _decode(errors.$ZodRealError);
 export type $EncodeAsync = <T extends schemas.$ZodType>(
   schema: T,
   value: core.output<T>,
-  _ctx?: schemas.ParseContext<errors.$ZodIssue>
+  _ctx?: schemas.ParseContext<errors.$ZodIssue>,
+  _params?: $ParseParams
 ) => Promise<core.input<T>>;
 
-export const _encodeAsync: (_Err: $ZodErrorClass) => $EncodeAsync = (_Err) => async (schema, value, _ctx) => {
-  const ctx = _ctx ? { ..._ctx, direction: "backward" as const } : { direction: "backward" as const };
-  return _parseAsync(_Err)(schema, value, ctx as any) as any;
+export const _encodeAsync: (_Err: $ZodErrorClass) => $EncodeAsync = (_Err) => {
+  const parseAsync = _parseAsync(_Err);
+  const fn: $EncodeAsync = async (schema, value, _ctx, _params) => {
+    const ctx = _ctx ? { ..._ctx, direction: "backward" as const } : { direction: "backward" as const };
+    return parseAsync(schema, value, ctx as any, finalizeParams(fn, _params)) as any;
+  };
+  return fn;
 };
 
 export const encodeAsync: $EncodeAsync = /* @__PURE__*/ _encodeAsync(errors.$ZodRealError);
@@ -135,11 +161,16 @@ export const encodeAsync: $EncodeAsync = /* @__PURE__*/ _encodeAsync(errors.$Zod
 export type $DecodeAsync = <T extends schemas.$ZodType>(
   schema: T,
   value: core.input<T>,
-  _ctx?: schemas.ParseContext<errors.$ZodIssue>
+  _ctx?: schemas.ParseContext<errors.$ZodIssue>,
+  _params?: $ParseParams
 ) => Promise<core.output<T>>;
 
-export const _decodeAsync: (_Err: $ZodErrorClass) => $DecodeAsync = (_Err) => async (schema, value, _ctx) => {
-  return _parseAsync(_Err)(schema, value, _ctx);
+export const _decodeAsync: (_Err: $ZodErrorClass) => $DecodeAsync = (_Err) => {
+  const parseAsync = _parseAsync(_Err);
+  const fn: $DecodeAsync = async (schema, value, _ctx, _params) => {
+    return parseAsync(schema, value, _ctx, finalizeParams(fn, _params));
+  };
+  return fn;
 };
 
 export const decodeAsync: $DecodeAsync = /* @__PURE__*/ _decodeAsync(errors.$ZodRealError);
