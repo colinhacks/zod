73c10c4632093a0495543e849cf4c9414de8b788
zirkelc <chris.zirkel@gmail.com>
2026-08-03 09:58:04 +0200

    perf(v4): prefix issue paths in place in object JIT failure path

--- packages/zod/src/v4/core/schemas.ts
+++ packages/zod/src/v4/core/schemas.ts
@@ -2165,10 +2165,11 @@ export const $ZodObjectJIT: core.$constructor<$ZodObject> = /*@__PURE__*/ core.$
         const ${id}_present = ${isPresent};
         if (!${id}.issues.length || ${id}_present) {
           if (${id}.issues.length) {
-            payload.issues = payload.issues.concat(${id}.issues.map(iss => ({
-              ...iss,
-              path: iss.path ? [${k}, ...iss.path] : [${k}]
-            })));
+            for (let i = 0; i < ${id}.issues.length; i++) {
+              const iss = ${id}.issues[i];
+              iss.path = iss.path ? [${k}, ...iss.path] : [${k}];
+              payload.issues.push(iss);
+            }
           }
 
           if (${assign}) {
@@ -2181,10 +2182,11 @@ export const $ZodObjectJIT: core.$constructor<$ZodObject> = /*@__PURE__*/ core.$
           doc.write(`
         const ${id}_present = ${isPresent};
         if (${id}.issues.length) {
-          payload.issues = payload.issues.concat(${id}.issues.map(iss => ({
-            ...iss,
-            path: iss.path ? [${k}, ...iss.path] : [${k}]
-          })));
+          for (let i = 0; i < ${id}.issues.length; i++) {
+            const iss = ${id}.issues[i];
+            iss.path = iss.path ? [${k}, ...iss.path] : [${k}];
+            payload.issues.push(iss);
+          }
         }
         if (!${id}_present && !${id}.issues.length) {
           payload.issues.push({
@@ -2203,10 +2205,11 @@ export const $ZodObjectJIT: core.$constructor<$ZodObject> = /*@__PURE__*/ core.$
         } else {
           doc.write(`
         if (${id}.issues.length) {
-          payload.issues = payload.issues.concat(${id}.issues.map(iss => ({
-            ...iss,
-            path: iss.path ? [${k}, ...iss.path] : [${k}]
-          })));
+          for (let i = 0; i < ${id}.issues.length; i++) {
+            const iss = ${id}.issues[i];
+            iss.path = iss.path ? [${k}, ...iss.path] : [${k}];
+            payload.issues.push(iss);
+          }
         }
         
         if (${id}.value === undefined) {
